ad944f47fbf8ab086aae73c7f9b3467d3d833c1c
Frank Shen <fshen01@fortinet.com>
2020-08-22 18:10:08 +0800

    Support ReportTheme

--- fortios/sdkcore/sdkfos.go
+++ fortios/sdkcore/sdkfos.go
@@ -13732,3 +13732,57 @@ func (c *FortiSDKClient) ReadReportChart(mkey string) (mapTmp map[string]interfa
 	return
 }
 
+// CreateReportStyle API operation for FortiOS creates a new Style.
+// Returns the index value of the Style and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the report - style chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) CreateReportStyle(params *map[string]interface{}) (output map[string]interface{}, err error) {
+
+	HTTPMethod := "POST"
+	path := "/api/v2/cmdb/report/style"
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// UpdateReportStyle API operation for FortiOS updates the specified Style.
+// Returns the index value of the Style and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the report - style chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) UpdateReportStyle(params *map[string]interface{}, mkey string) (output map[string]interface{}, err error) {
+	HTTPMethod := "PUT"
+	path := "/api/v2/cmdb/report/style"
+	path += "/" + escapeURLString(mkey)
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// DeleteReportStyle API operation for FortiOS deletes the specified Style.
+// Returns error for service API and SDK errors.
+// See the report - style chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) DeleteReportStyle(mkey string) (err error) {
+	HTTPMethod := "DELETE"
+	path := "/api/v2/cmdb/report/style"
+	path += "/" + escapeURLString(mkey)
+
+	err = delete(c, HTTPMethod, path)
+	return
+}
+
+// ReadReportStyle API operation for FortiOS gets the Style
+// with the specified index value.
+// Returns the requested Style value when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the report - style chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) ReadReportStyle(mkey string) (mapTmp map[string]interface{}, err error) {
+	HTTPMethod := "GET"
+	path := "/api/v2/cmdb/report/style"
+	path += "/" + escapeURLString(mkey)
+
+	mapTmp, err = read(c, HTTPMethod, path, false)
+	return
+}
+
