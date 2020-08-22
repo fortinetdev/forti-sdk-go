2c793effdab82e50cbef63f6e288d997afe380c6
Frank Shen <fshen01@fortinet.com>
2020-08-22 18:01:25 +0800

    Support LogDiskFilter

--- fortios/sdkcore/sdkfos.go
+++ fortios/sdkcore/sdkfos.go
@@ -11429,3 +11429,39 @@ func (c *FortiSDKClient) ReadLogMemoryFilter(mkey string) (mapTmp map[string]int
 }
 
 
+// UpdateLogDiskSetting API operation for FortiOS updates the specified Setting.
+// Returns the index value of the Setting and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the log.disk - setting chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) UpdateLogDiskSetting(params *map[string]interface{}, mkey string) (output map[string]interface{}, err error) {
+	HTTPMethod := "PUT"
+	path := "/api/v2/cmdb/log.disk/setting"
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// DeleteLogDiskSetting API operation for FortiOS deletes the specified Setting.
+// Returns error for service API and SDK errors.
+// See the log.disk - setting chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) DeleteLogDiskSetting(mkey string) (err error) {
+
+	//No unset API for log.disk - setting
+	return
+}
+
+// ReadLogDiskSetting API operation for FortiOS gets the Setting
+// with the specified index value.
+// Returns the requested Setting value when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the log.disk - setting chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) ReadLogDiskSetting(mkey string) (mapTmp map[string]interface{}, err error) {
+	HTTPMethod := "GET"
+	path := "/api/v2/cmdb/log.disk/setting"
+
+	mapTmp, err = read(c, HTTPMethod, path, true)
+	return
+}
+
+
