30028776f4dd052e1efeb0484daf91edcf83a343
Frank Shen <fshen01@fortinet.com>
2020-08-22 18:02:46 +0800

    Support LogSetting

--- fortios/sdkcore/sdkfos.go
+++ fortios/sdkcore/sdkfos.go
@@ -11717,3 +11717,39 @@ func (c *FortiSDKClient) ReadLogNullDeviceSetting(mkey string) (mapTmp map[strin
 }
 
 
+// UpdateLogNullDeviceFilter API operation for FortiOS updates the specified Filter.
+// Returns the index value of the Filter and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the log.null-device - filter chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) UpdateLogNullDeviceFilter(params *map[string]interface{}, mkey string) (output map[string]interface{}, err error) {
+	HTTPMethod := "PUT"
+	path := "/api/v2/cmdb/log.null-device/filter"
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// DeleteLogNullDeviceFilter API operation for FortiOS deletes the specified Filter.
+// Returns error for service API and SDK errors.
+// See the log.null-device - filter chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) DeleteLogNullDeviceFilter(mkey string) (err error) {
+
+	//No unset API for log.null-device - filter
+	return
+}
+
+// ReadLogNullDeviceFilter API operation for FortiOS gets the Filter
+// with the specified index value.
+// Returns the requested Filter value when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the log.null-device - filter chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) ReadLogNullDeviceFilter(mkey string) (mapTmp map[string]interface{}, err error) {
+	HTTPMethod := "GET"
+	path := "/api/v2/cmdb/log.null-device/filter"
+
+	mapTmp, err = read(c, HTTPMethod, path, true)
+	return
+}
+
+
