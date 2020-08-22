bd774e808d99891d6e1de93822de366896bba4c6
Frank Shen <fshen01@fortinet.com>
2020-08-22 17:53:13 +0800

    Support WebProxyProfile

--- fortios/sdkcore/sdkfos.go
+++ fortios/sdkcore/sdkfos.go
@@ -9287,3 +9287,38 @@ func (c *FortiSDKClient) ReadIpsGlobal(mkey string) (mapTmp map[string]interface
 }
 
 
+// UpdateIpsSettings API operation for FortiOS updates the specified Settings.
+// Returns the index value of the Settings and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the ips - settings chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) UpdateIpsSettings(params *map[string]interface{}, mkey string) (output map[string]interface{}, err error) {
+	HTTPMethod := "PUT"
+	path := "/api/v2/cmdb/ips/settings"
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// DeleteIpsSettings API operation for FortiOS deletes the specified Settings.
+// Returns error for service API and SDK errors.
+// See the ips - settings chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) DeleteIpsSettings(mkey string) (err error) {
+
+	//No unset API for ips - settings
+	return
+}
+
+// ReadIpsSettings API operation for FortiOS gets the Settings
+// with the specified index value.
+// Returns the requested Settings value when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the ips - settings chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) ReadIpsSettings(mkey string) (mapTmp map[string]interface{}, err error) {
+	HTTPMethod := "GET"
+	path := "/api/v2/cmdb/ips/settings"
+
+	mapTmp, err = read(c, HTTPMethod, path, true)
+	return
+}
+
