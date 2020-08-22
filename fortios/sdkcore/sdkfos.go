dbeabc332634fbc22d5a70a8c7bf9916480f8513
Frank Shen <fshen01@fortinet.com>
2020-08-22 18:04:16 +0800

    Support LogFortianalyzer2OverrideFilter

--- fortios/sdkcore/sdkfos.go
+++ fortios/sdkcore/sdkfos.go
@@ -12041,3 +12041,39 @@ func (c *FortiSDKClient) ReadLogFortianalyzer2OverrideSetting(mkey string) (mapT
 }
 
 
+// UpdateLogFortianalyzer2Filter API operation for FortiOS updates the specified Filter.
+// Returns the index value of the Filter and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the log.fortianalyzer2 - filter chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) UpdateLogFortianalyzer2Filter(params *map[string]interface{}, mkey string) (output map[string]interface{}, err error) {
+	HTTPMethod := "PUT"
+	path := "/api/v2/cmdb/log.fortianalyzer2/filter"
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// DeleteLogFortianalyzer2Filter API operation for FortiOS deletes the specified Filter.
+// Returns error for service API and SDK errors.
+// See the log.fortianalyzer2 - filter chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) DeleteLogFortianalyzer2Filter(mkey string) (err error) {
+
+	//No unset API for log.fortianalyzer2 - filter
+	return
+}
+
+// ReadLogFortianalyzer2Filter API operation for FortiOS gets the Filter
+// with the specified index value.
+// Returns the requested Filter value when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the log.fortianalyzer2 - filter chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) ReadLogFortianalyzer2Filter(mkey string) (mapTmp map[string]interface{}, err error) {
+	HTTPMethod := "GET"
+	path := "/api/v2/cmdb/log.fortianalyzer2/filter"
+
+	mapTmp, err = read(c, HTTPMethod, path, true)
+	return
+}
+
+
