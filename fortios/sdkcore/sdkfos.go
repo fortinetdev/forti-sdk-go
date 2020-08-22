24a1a23f92cdbeb0b3f182c299213236b1bc28dc
Frank Shen <fshen01@fortinet.com>
2020-08-22 18:10:38 +0800

    Support WafProfile

--- fortios/sdkcore/sdkfos.go
+++ fortios/sdkcore/sdkfos.go
@@ -13895,3 +13895,38 @@ func (c *FortiSDKClient) ReadReportLayout(mkey string) (mapTmp map[string]interf
 }
 
 
+// UpdateReportSetting API operation for FortiOS updates the specified Setting.
+// Returns the index value of the Setting and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the report - setting chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) UpdateReportSetting(params *map[string]interface{}, mkey string) (output map[string]interface{}, err error) {
+	HTTPMethod := "PUT"
+	path := "/api/v2/cmdb/report/setting"
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// DeleteReportSetting API operation for FortiOS deletes the specified Setting.
+// Returns error for service API and SDK errors.
+// See the report - setting chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) DeleteReportSetting(mkey string) (err error) {
+
+	//No unset API for report - setting
+	return
+}
+
+// ReadReportSetting API operation for FortiOS gets the Setting
+// with the specified index value.
+// Returns the requested Setting value when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the report - setting chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) ReadReportSetting(mkey string) (mapTmp map[string]interface{}, err error) {
+	HTTPMethod := "GET"
+	path := "/api/v2/cmdb/report/setting"
+
+	mapTmp, err = read(c, HTTPMethod, path, true)
+	return
+}
+
