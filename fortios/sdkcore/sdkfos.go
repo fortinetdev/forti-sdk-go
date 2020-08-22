259662f87ba1c964b359d12360a0899b4bb93944
Frank Shen <fshen01@fortinet.com>
2020-08-22 17:31:48 +0800

    Support SystemAutoInstall

--- fortios/sdkcore/sdkfos.go
+++ fortios/sdkcore/sdkfos.go
@@ -2878,3 +2878,58 @@ func (c *FortiSDKClient) ReadSystemProbeResponse(mkey string) (mapTmp map[string
 	return
 }
 
+// CreateSystemLinkMonitor API operation for FortiOS creates a new Link Monitor.
+// Returns the index value of the Link Monitor and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the system - link-monitor chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) CreateSystemLinkMonitor(params *map[string]interface{}) (output map[string]interface{}, err error) {
+
+	HTTPMethod := "POST"
+	path := "/api/v2/cmdb/system/link-monitor"
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// UpdateSystemLinkMonitor API operation for FortiOS updates the specified Link Monitor.
+// Returns the index value of the Link Monitor and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the system - link-monitor chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) UpdateSystemLinkMonitor(params *map[string]interface{}, mkey string) (output map[string]interface{}, err error) {
+	HTTPMethod := "PUT"
+	path := "/api/v2/cmdb/system/link-monitor"
+	path += "/" + escapeURLString(mkey)
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// DeleteSystemLinkMonitor API operation for FortiOS deletes the specified Link Monitor.
+// Returns error for service API and SDK errors.
+// See the system - link-monitor chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) DeleteSystemLinkMonitor(mkey string) (err error) {
+	HTTPMethod := "DELETE"
+	path := "/api/v2/cmdb/system/link-monitor"
+	path += "/" + escapeURLString(mkey)
+
+	err = delete(c, HTTPMethod, path)
+	return
+}
+
+// ReadSystemLinkMonitor API operation for FortiOS gets the Link Monitor
+// with the specified index value.
+// Returns the requested Link Monitor value when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the system - link-monitor chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) ReadSystemLinkMonitor(mkey string) (mapTmp map[string]interface{}, err error) {
+	HTTPMethod := "GET"
+	path := "/api/v2/cmdb/system/link-monitor"
+	path += "/" + escapeURLString(mkey)
+
+	mapTmp, err = read(c, HTTPMethod, path, false)
+	return
+}
+
+
