eebefca3e35fe870d85127f44889a9a32434b3cc
Frank Shen <fshen01@fortinet.com>
2020-08-22 17:52:33 +0800

    Support WebfilterFtgdLocalRating

--- fortios/sdkcore/sdkfos.go
+++ fortios/sdkcore/sdkfos.go
@@ -9088,3 +9088,57 @@ func (c *FortiSDKClient) ReadWebfilterFortiguard(mkey string) (mapTmp map[string
 	return
 }
 
+// CreateWebfilterOverride API operation for FortiOS creates a new Override.
+// Returns the index value of the Override and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the webfilter - override chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) CreateWebfilterOverride(params *map[string]interface{}) (output map[string]interface{}, err error) {
+
+	HTTPMethod := "POST"
+	path := "/api/v2/cmdb/webfilter/override"
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// UpdateWebfilterOverride API operation for FortiOS updates the specified Override.
+// Returns the index value of the Override and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the webfilter - override chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) UpdateWebfilterOverride(params *map[string]interface{}, mkey string) (output map[string]interface{}, err error) {
+	HTTPMethod := "PUT"
+	path := "/api/v2/cmdb/webfilter/override"
+	path += "/" + escapeURLString(mkey)
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// DeleteWebfilterOverride API operation for FortiOS deletes the specified Override.
+// Returns error for service API and SDK errors.
+// See the webfilter - override chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) DeleteWebfilterOverride(mkey string) (err error) {
+	HTTPMethod := "DELETE"
+	path := "/api/v2/cmdb/webfilter/override"
+	path += "/" + escapeURLString(mkey)
+
+	err = delete(c, HTTPMethod, path)
+	return
+}
+
+// ReadWebfilterOverride API operation for FortiOS gets the Override
+// with the specified index value.
+// Returns the requested Override value when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the webfilter - override chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) ReadWebfilterOverride(mkey string) (mapTmp map[string]interface{}, err error) {
+	HTTPMethod := "GET"
+	path := "/api/v2/cmdb/webfilter/override"
+	path += "/" + escapeURLString(mkey)
+
+	mapTmp, err = read(c, HTTPMethod, path, false)
+	return
+}
+
