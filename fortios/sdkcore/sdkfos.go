a0739d76bd764448772b4ea779c2a89d9252a5ea
Frank Shen <fshen01@fortinet.com>
2020-08-22 17:21:26 +0800

    Support SystemAccprofile

---
+++ fortios/sdkcore/sdkfos.go
@@ -0,0 +1,54 @@
+// Copyright 2020 Fortinet, Inc. All rights reserved.
+// Author: Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu)
+// Documentation:
+// Frank Shen (@frankshen01), Hongbin Lu (@fgtdev-hblu),
+// Yuffie Zhu (@yuffiezhu), Yue Wang (@yuew-ftnt)
+
+// Description: Description: SDK for FortiOS Provider
+
+package forticlient
+
+type creatUpdateOutput struct {
+	Vdom       string  `json:"vdom"`
+	Mkey       string  `json:"mkey"`
+	Status     string  `json:"status"`
+	HTTPStatus float64 `json:"http_status"`
+}
+
+
+
+// UpdateSystemGlobal API operation for FortiOS updates the specified Global.
+// Returns the index value of the Global and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the system - global chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) UpdateSystemGlobal(params *map[string]interface{}, mkey string) (output map[string]interface{}, err error) {
+	HTTPMethod := "PUT"
+	path := "/api/v2/cmdb/system/global"
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// DeleteSystemGlobal API operation for FortiOS deletes the specified Global.
+// Returns error for service API and SDK errors.
+// See the system - global chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) DeleteSystemGlobal(mkey string) (err error) {
+
+	//No unset API for system - global
+	return
+}
+
+// ReadSystemGlobal API operation for FortiOS gets the Global
+// with the specified index value.
+// Returns the requested Global value when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the system - global chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) ReadSystemGlobal(mkey string) (mapTmp map[string]interface{}, err error) {
+	HTTPMethod := "GET"
+	path := "/api/v2/cmdb/system/global"
+
+	mapTmp, err = read(c, HTTPMethod, path, true)
+	return
+}
+
