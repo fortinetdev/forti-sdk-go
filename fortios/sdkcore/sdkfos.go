03ae9341487878f11a14b35604ba8462f3fe8b10
Frank Shen <fshen01@fortinet.com>
2020-08-22 18:12:09 +0800

    Support RouterAccessList6

--- fortios/sdkcore/sdkfos.go
+++ fortios/sdkcore/sdkfos.go
@@ -14308,3 +14308,57 @@ func (c *FortiSDKClient) ReadAlertemailSetting(mkey string) (mapTmp map[string]i
 	return
 }
 
+// CreateRouterAccessList API operation for FortiOS creates a new Access List.
+// Returns the index value of the Access List and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the router - access-list chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) CreateRouterAccessList(params *map[string]interface{}) (output map[string]interface{}, err error) {
+
+	HTTPMethod := "POST"
+	path := "/api/v2/cmdb/router/access-list"
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// UpdateRouterAccessList API operation for FortiOS updates the specified Access List.
+// Returns the index value of the Access List and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the router - access-list chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) UpdateRouterAccessList(params *map[string]interface{}, mkey string) (output map[string]interface{}, err error) {
+	HTTPMethod := "PUT"
+	path := "/api/v2/cmdb/router/access-list"
+	path += "/" + escapeURLString(mkey)
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// DeleteRouterAccessList API operation for FortiOS deletes the specified Access List.
+// Returns error for service API and SDK errors.
+// See the router - access-list chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) DeleteRouterAccessList(mkey string) (err error) {
+	HTTPMethod := "DELETE"
+	path := "/api/v2/cmdb/router/access-list"
+	path += "/" + escapeURLString(mkey)
+
+	err = delete(c, HTTPMethod, path)
+	return
+}
+
+// ReadRouterAccessList API operation for FortiOS gets the Access List
+// with the specified index value.
+// Returns the requested Access List value when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the router - access-list chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) ReadRouterAccessList(mkey string) (mapTmp map[string]interface{}, err error) {
+	HTTPMethod := "GET"
+	path := "/api/v2/cmdb/router/access-list"
+	path += "/" + escapeURLString(mkey)
+
+	mapTmp, err = read(c, HTTPMethod, path, false)
+	return
+}
+
