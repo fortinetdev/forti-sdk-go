baf0981a56a5283925ccd172f1d7209107553d73
Frank Shen <fshen01@fortinet.com>
2020-08-22 18:14:50 +0800

    Support RouterMulticastFlow

--- fortios/sdkcore/sdkfos.go
+++ fortios/sdkcore/sdkfos.go
@@ -15083,3 +15083,38 @@ func (c *FortiSDKClient) ReadRouterBgp(mkey string) (mapTmp map[string]interface
 }
 
 
+// UpdateRouterIsis API operation for FortiOS updates the specified Isis.
+// Returns the index value of the Isis and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the router - isis chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) UpdateRouterIsis(params *map[string]interface{}, mkey string) (output map[string]interface{}, err error) {
+	HTTPMethod := "PUT"
+	path := "/api/v2/cmdb/router/isis"
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// DeleteRouterIsis API operation for FortiOS deletes the specified Isis.
+// Returns error for service API and SDK errors.
+// See the router - isis chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) DeleteRouterIsis(mkey string) (err error) {
+
+	//No unset API for router - isis
+	return
+}
+
+// ReadRouterIsis API operation for FortiOS gets the Isis
+// with the specified index value.
+// Returns the requested Isis value when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the router - isis chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) ReadRouterIsis(mkey string) (mapTmp map[string]interface{}, err error) {
+	HTTPMethod := "GET"
+	path := "/api/v2/cmdb/router/isis"
+
+	mapTmp, err = read(c, HTTPMethod, path, true)
+	return
+}
+
