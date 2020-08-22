4a803a016ea1def80674de680fa776b8b48b2852
Frank Shen <fshen01@fortinet.com>
2020-08-22 18:12:29 +0800

    Support RouterPrefixList6

--- fortios/sdkcore/sdkfos.go
+++ fortios/sdkcore/sdkfos.go
@@ -14416,3 +14416,57 @@ func (c *FortiSDKClient) ReadRouterAccessList6(mkey string) (mapTmp map[string]i
 	return
 }
 
+// CreateRouterPrefixList API operation for FortiOS creates a new Prefix List.
+// Returns the index value of the Prefix List and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the router - prefix-list chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) CreateRouterPrefixList(params *map[string]interface{}) (output map[string]interface{}, err error) {
+
+	HTTPMethod := "POST"
+	path := "/api/v2/cmdb/router/prefix-list"
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// UpdateRouterPrefixList API operation for FortiOS updates the specified Prefix List.
+// Returns the index value of the Prefix List and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the router - prefix-list chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) UpdateRouterPrefixList(params *map[string]interface{}, mkey string) (output map[string]interface{}, err error) {
+	HTTPMethod := "PUT"
+	path := "/api/v2/cmdb/router/prefix-list"
+	path += "/" + escapeURLString(mkey)
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// DeleteRouterPrefixList API operation for FortiOS deletes the specified Prefix List.
+// Returns error for service API and SDK errors.
+// See the router - prefix-list chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) DeleteRouterPrefixList(mkey string) (err error) {
+	HTTPMethod := "DELETE"
+	path := "/api/v2/cmdb/router/prefix-list"
+	path += "/" + escapeURLString(mkey)
+
+	err = delete(c, HTTPMethod, path)
+	return
+}
+
+// ReadRouterPrefixList API operation for FortiOS gets the Prefix List
+// with the specified index value.
+// Returns the requested Prefix List value when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the router - prefix-list chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) ReadRouterPrefixList(mkey string) (mapTmp map[string]interface{}, err error) {
+	HTTPMethod := "GET"
+	path := "/api/v2/cmdb/router/prefix-list"
+	path += "/" + escapeURLString(mkey)
+
+	mapTmp, err = read(c, HTTPMethod, path, false)
+	return
+}
+
