a7ce6edeac1c6147311d31e320d66cc8c73a8b0f
Frank Shen <fshen01@fortinet.com>
2020-08-22 17:42:40 +0800

    Support FirewallVip46

--- fortios/sdkcore/sdkfos.go
+++ fortios/sdkcore/sdkfos.go
@@ -6082,3 +6082,57 @@ func (c *FortiSDKClient) ReadFirewallLdbMonitor(mkey string) (mapTmp map[string]
 	return
 }
 
+// CreateFirewallVip API operation for FortiOS creates a new Vip.
+// Returns the index value of the Vip and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the firewall - vip chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) CreateFirewallVip(params *map[string]interface{}) (output map[string]interface{}, err error) {
+
+	HTTPMethod := "POST"
+	path := "/api/v2/cmdb/firewall/vip"
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// UpdateFirewallVip API operation for FortiOS updates the specified Vip.
+// Returns the index value of the Vip and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the firewall - vip chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) UpdateFirewallVip(params *map[string]interface{}, mkey string) (output map[string]interface{}, err error) {
+	HTTPMethod := "PUT"
+	path := "/api/v2/cmdb/firewall/vip"
+	path += "/" + escapeURLString(mkey)
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// DeleteFirewallVip API operation for FortiOS deletes the specified Vip.
+// Returns error for service API and SDK errors.
+// See the firewall - vip chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) DeleteFirewallVip(mkey string) (err error) {
+	HTTPMethod := "DELETE"
+	path := "/api/v2/cmdb/firewall/vip"
+	path += "/" + escapeURLString(mkey)
+
+	err = delete(c, HTTPMethod, path)
+	return
+}
+
+// ReadFirewallVip API operation for FortiOS gets the Vip
+// with the specified index value.
+// Returns the requested Vip value when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the firewall - vip chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) ReadFirewallVip(mkey string) (mapTmp map[string]interface{}, err error) {
+	HTTPMethod := "GET"
+	path := "/api/v2/cmdb/firewall/vip"
+	path += "/" + escapeURLString(mkey)
+
+	mapTmp, err = read(c, HTTPMethod, path, false)
+	return
+}
+
