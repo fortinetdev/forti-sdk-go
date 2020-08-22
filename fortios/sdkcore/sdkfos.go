5d87394e26ea434cb40f4ebd943b6797b452978e
Frank Shen <fshen01@fortinet.com>
2020-08-22 17:24:46 +0800

    Support SystemReplacemsgImage

--- fortios/sdkcore/sdkfos.go
+++ fortios/sdkcore/sdkfos.go
@@ -917,3 +917,38 @@ func (c *FortiSDKClient) ReadSystemNetflow(mkey string) (mapTmp map[string]inter
 }
 
 
+// UpdateSystemVdomNetflow API operation for FortiOS updates the specified Vdom Netflow.
+// Returns the index value of the Vdom Netflow and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the system - vdom-netflow chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) UpdateSystemVdomNetflow(params *map[string]interface{}, mkey string) (output map[string]interface{}, err error) {
+	HTTPMethod := "PUT"
+	path := "/api/v2/cmdb/system/vdom-netflow"
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// DeleteSystemVdomNetflow API operation for FortiOS deletes the specified Vdom Netflow.
+// Returns error for service API and SDK errors.
+// See the system - vdom-netflow chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) DeleteSystemVdomNetflow(mkey string) (err error) {
+
+	//No unset API for system - vdom-netflow
+	return
+}
+
+// ReadSystemVdomNetflow API operation for FortiOS gets the Vdom Netflow
+// with the specified index value.
+// Returns the requested Vdom Netflow value when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the system - vdom-netflow chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) ReadSystemVdomNetflow(mkey string) (mapTmp map[string]interface{}, err error) {
+	HTTPMethod := "GET"
+	path := "/api/v2/cmdb/system/vdom-netflow"
+
+	mapTmp, err = read(c, HTTPMethod, path, true)
+	return
+}
+
