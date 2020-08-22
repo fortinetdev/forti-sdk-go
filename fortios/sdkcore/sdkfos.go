c1e3914905407923abaa6c46b92d993cb233dc6f
Frank Shen <fshen01@fortinet.com>
2020-08-22 17:54:33 +0800

    Support WanoptSettings

--- fortios/sdkcore/sdkfos.go
+++ fortios/sdkcore/sdkfos.go
@@ -9683,3 +9683,39 @@ func (c *FortiSDKClient) ReadWebProxyUrlMatch(mkey string) (mapTmp map[string]in
 }
 
 
+// UpdateWanoptWebcache API operation for FortiOS updates the specified Webcache.
+// Returns the index value of the Webcache and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the wanopt - webcache chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) UpdateWanoptWebcache(params *map[string]interface{}, mkey string) (output map[string]interface{}, err error) {
+	HTTPMethod := "PUT"
+	path := "/api/v2/cmdb/wanopt/webcache"
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// DeleteWanoptWebcache API operation for FortiOS deletes the specified Webcache.
+// Returns error for service API and SDK errors.
+// See the wanopt - webcache chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) DeleteWanoptWebcache(mkey string) (err error) {
+
+	//No unset API for wanopt - webcache
+	return
+}
+
+// ReadWanoptWebcache API operation for FortiOS gets the Webcache
+// with the specified index value.
+// Returns the requested Webcache value when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the wanopt - webcache chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) ReadWanoptWebcache(mkey string) (mapTmp map[string]interface{}, err error) {
+	HTTPMethod := "GET"
+	path := "/api/v2/cmdb/wanopt/webcache"
+
+	mapTmp, err = read(c, HTTPMethod, path, true)
+	return
+}
+
+
