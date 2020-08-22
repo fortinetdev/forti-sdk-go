54412f911bded60f88b94a87b5cd549b0d4bd6b1
Frank Shen <fshen01@fortinet.com>
2020-08-22 17:35:39 +0800

    Support WirelessControllerHotspot20Icon

--- fortios/sdkcore/sdkfos.go
+++ fortios/sdkcore/sdkfos.go
@@ -3940,3 +3940,57 @@ func (c *FortiSDKClient) ReadWirelessControllerHotspot20H2QpWanMetric(mkey strin
 	return
 }
 
+// CreateWirelessControllerHotspot20H2QpConnCapability API operation for FortiOS creates a new H2Qp Conn Capability.
+// Returns the index value of the H2Qp Conn Capability and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the wireless-controller.hotspot20 - h2qp-conn-capability chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) CreateWirelessControllerHotspot20H2QpConnCapability(params *map[string]interface{}) (output map[string]interface{}, err error) {
+
+	HTTPMethod := "POST"
+	path := "/api/v2/cmdb/wireless-controller.hotspot20/h2qp-conn-capability"
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// UpdateWirelessControllerHotspot20H2QpConnCapability API operation for FortiOS updates the specified H2Qp Conn Capability.
+// Returns the index value of the H2Qp Conn Capability and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the wireless-controller.hotspot20 - h2qp-conn-capability chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) UpdateWirelessControllerHotspot20H2QpConnCapability(params *map[string]interface{}, mkey string) (output map[string]interface{}, err error) {
+	HTTPMethod := "PUT"
+	path := "/api/v2/cmdb/wireless-controller.hotspot20/h2qp-conn-capability"
+	path += "/" + escapeURLString(mkey)
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// DeleteWirelessControllerHotspot20H2QpConnCapability API operation for FortiOS deletes the specified H2Qp Conn Capability.
+// Returns error for service API and SDK errors.
+// See the wireless-controller.hotspot20 - h2qp-conn-capability chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) DeleteWirelessControllerHotspot20H2QpConnCapability(mkey string) (err error) {
+	HTTPMethod := "DELETE"
+	path := "/api/v2/cmdb/wireless-controller.hotspot20/h2qp-conn-capability"
+	path += "/" + escapeURLString(mkey)
+
+	err = delete(c, HTTPMethod, path)
+	return
+}
+
+// ReadWirelessControllerHotspot20H2QpConnCapability API operation for FortiOS gets the H2Qp Conn Capability
+// with the specified index value.
+// Returns the requested H2Qp Conn Capability value when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the wireless-controller.hotspot20 - h2qp-conn-capability chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) ReadWirelessControllerHotspot20H2QpConnCapability(mkey string) (mapTmp map[string]interface{}, err error) {
+	HTTPMethod := "GET"
+	path := "/api/v2/cmdb/wireless-controller.hotspot20/h2qp-conn-capability"
+	path += "/" + escapeURLString(mkey)
+
+	mapTmp, err = read(c, HTTPMethod, path, false)
+	return
+}
+
