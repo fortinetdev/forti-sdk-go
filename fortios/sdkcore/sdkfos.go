ca3b280c3021075c28ab273fbdec5d1a3978bfae
Frank Shen <fshen01@fortinet.com>
2020-08-22 17:56:04 +0800

    Support DlpFilepattern

--- fortios/sdkcore/sdkfos.go
+++ fortios/sdkcore/sdkfos.go
@@ -10078,3 +10078,57 @@ func (c *FortiSDKClient) ReadApplicationList(mkey string) (mapTmp map[string]int
 	return
 }
 
+// CreateApplicationGroup API operation for FortiOS creates a new Group.
+// Returns the index value of the Group and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the application - group chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) CreateApplicationGroup(params *map[string]interface{}) (output map[string]interface{}, err error) {
+
+	HTTPMethod := "POST"
+	path := "/api/v2/cmdb/application/group"
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// UpdateApplicationGroup API operation for FortiOS updates the specified Group.
+// Returns the index value of the Group and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the application - group chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) UpdateApplicationGroup(params *map[string]interface{}, mkey string) (output map[string]interface{}, err error) {
+	HTTPMethod := "PUT"
+	path := "/api/v2/cmdb/application/group"
+	path += "/" + escapeURLString(mkey)
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// DeleteApplicationGroup API operation for FortiOS deletes the specified Group.
+// Returns error for service API and SDK errors.
+// See the application - group chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) DeleteApplicationGroup(mkey string) (err error) {
+	HTTPMethod := "DELETE"
+	path := "/api/v2/cmdb/application/group"
+	path += "/" + escapeURLString(mkey)
+
+	err = delete(c, HTTPMethod, path)
+	return
+}
+
+// ReadApplicationGroup API operation for FortiOS gets the Group
+// with the specified index value.
+// Returns the requested Group value when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the application - group chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) ReadApplicationGroup(mkey string) (mapTmp map[string]interface{}, err error) {
+	HTTPMethod := "GET"
+	path := "/api/v2/cmdb/application/group"
+	path += "/" + escapeURLString(mkey)
+
+	mapTmp, err = read(c, HTTPMethod, path, false)
+	return
+}
+
