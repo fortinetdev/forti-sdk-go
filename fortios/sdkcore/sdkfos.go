f0fab225959b65e6aa460ea0903c98e0b5bc619b
Frank Shen <fshen01@fortinet.com>
2020-08-22 17:34:19 +0800

    Support WirelessControllerHotspot20AnqpVenueName

--- fortios/sdkcore/sdkfos.go
+++ fortios/sdkcore/sdkfos.go
@@ -3527,3 +3527,38 @@ func (c *FortiSDKClient) ReadWirelessControllerInterController(mkey string) (map
 }
 
 
+// UpdateWirelessControllerGlobal API operation for FortiOS updates the specified Global.
+// Returns the index value of the Global and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the wireless-controller - global chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) UpdateWirelessControllerGlobal(params *map[string]interface{}, mkey string) (output map[string]interface{}, err error) {
+	HTTPMethod := "PUT"
+	path := "/api/v2/cmdb/wireless-controller/global"
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// DeleteWirelessControllerGlobal API operation for FortiOS deletes the specified Global.
+// Returns error for service API and SDK errors.
+// See the wireless-controller - global chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) DeleteWirelessControllerGlobal(mkey string) (err error) {
+
+	//No unset API for wireless-controller - global
+	return
+}
+
+// ReadWirelessControllerGlobal API operation for FortiOS gets the Global
+// with the specified index value.
+// Returns the requested Global value when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the wireless-controller - global chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) ReadWirelessControllerGlobal(mkey string) (mapTmp map[string]interface{}, err error) {
+	HTTPMethod := "GET"
+	path := "/api/v2/cmdb/wireless-controller/global"
+
+	mapTmp, err = read(c, HTTPMethod, path, true)
+	return
+}
+
