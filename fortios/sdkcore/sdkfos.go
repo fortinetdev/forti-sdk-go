b409294836b3dfe2e7a42790dbaff3c8e69dbbfc
Frank Shen <fshen01@fortinet.com>
2020-08-22 17:22:56 +0800

    Support SystemFssoPolling

--- fortios/sdkcore/sdkfos.go
+++ fortios/sdkcore/sdkfos.go
@@ -448,3 +448,58 @@ func (c *FortiSDKClient) ReadSystemApiUser(mkey string) (mapTmp map[string]inter
 	return
 }
 
+// CreateSystemSitTunnel API operation for FortiOS creates a new Sit Tunnel.
+// Returns the index value of the Sit Tunnel and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the system - sit-tunnel chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) CreateSystemSitTunnel(params *map[string]interface{}) (output map[string]interface{}, err error) {
+
+	HTTPMethod := "POST"
+	path := "/api/v2/cmdb/system/sit-tunnel"
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// UpdateSystemSitTunnel API operation for FortiOS updates the specified Sit Tunnel.
+// Returns the index value of the Sit Tunnel and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the system - sit-tunnel chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) UpdateSystemSitTunnel(params *map[string]interface{}, mkey string) (output map[string]interface{}, err error) {
+	HTTPMethod := "PUT"
+	path := "/api/v2/cmdb/system/sit-tunnel"
+	path += "/" + escapeURLString(mkey)
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// DeleteSystemSitTunnel API operation for FortiOS deletes the specified Sit Tunnel.
+// Returns error for service API and SDK errors.
+// See the system - sit-tunnel chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) DeleteSystemSitTunnel(mkey string) (err error) {
+	HTTPMethod := "DELETE"
+	path := "/api/v2/cmdb/system/sit-tunnel"
+	path += "/" + escapeURLString(mkey)
+
+	err = delete(c, HTTPMethod, path)
+	return
+}
+
+// ReadSystemSitTunnel API operation for FortiOS gets the Sit Tunnel
+// with the specified index value.
+// Returns the requested Sit Tunnel value when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the system - sit-tunnel chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) ReadSystemSitTunnel(mkey string) (mapTmp map[string]interface{}, err error) {
+	HTTPMethod := "GET"
+	path := "/api/v2/cmdb/system/sit-tunnel"
+	path += "/" + escapeURLString(mkey)
+
+	mapTmp, err = read(c, HTTPMethod, path, false)
+	return
+}
+
+
