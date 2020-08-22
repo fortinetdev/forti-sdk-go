f1dc25302f0d1df2659f031be643160aa65f3096
Frank Shen <fshen01@fortinet.com>
2020-08-22 17:22:46 +0800

    Support SystemSitTunnel

--- fortios/sdkcore/sdkfos.go
+++ fortios/sdkcore/sdkfos.go
@@ -394,3 +394,57 @@ func (c *FortiSDKClient) ReadSystemAdmin(mkey string) (mapTmp map[string]interfa
 	return
 }
 
+// CreateSystemApiUser API operation for FortiOS creates a new Api User.
+// Returns the index value of the Api User and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the system - api-user chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) CreateSystemApiUser(params *map[string]interface{}) (output map[string]interface{}, err error) {
+
+	HTTPMethod := "POST"
+	path := "/api/v2/cmdb/system/api-user"
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// UpdateSystemApiUser API operation for FortiOS updates the specified Api User.
+// Returns the index value of the Api User and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the system - api-user chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) UpdateSystemApiUser(params *map[string]interface{}, mkey string) (output map[string]interface{}, err error) {
+	HTTPMethod := "PUT"
+	path := "/api/v2/cmdb/system/api-user"
+	path += "/" + escapeURLString(mkey)
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// DeleteSystemApiUser API operation for FortiOS deletes the specified Api User.
+// Returns error for service API and SDK errors.
+// See the system - api-user chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) DeleteSystemApiUser(mkey string) (err error) {
+	HTTPMethod := "DELETE"
+	path := "/api/v2/cmdb/system/api-user"
+	path += "/" + escapeURLString(mkey)
+
+	err = delete(c, HTTPMethod, path)
+	return
+}
+
+// ReadSystemApiUser API operation for FortiOS gets the Api User
+// with the specified index value.
+// Returns the requested Api User value when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the system - api-user chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) ReadSystemApiUser(mkey string) (mapTmp map[string]interface{}, err error) {
+	HTTPMethod := "GET"
+	path := "/api/v2/cmdb/system/api-user"
+	path += "/" + escapeURLString(mkey)
+
+	mapTmp, err = read(c, HTTPMethod, path, false)
+	return
+}
+
