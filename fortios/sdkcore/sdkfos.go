c7bb24baf33c98881e5cb806cf66147fe5f979d5
Frank Shen <fshen01@fortinet.com>
2020-08-22 17:44:21 +0800

    Support FirewallSslServer

--- fortios/sdkcore/sdkfos.go
+++ fortios/sdkcore/sdkfos.go
@@ -6604,3 +6604,57 @@ func (c *FortiSDKClient) ReadFirewallIpmacbindingTable(mkey string) (mapTmp map[
 	return
 }
 
+// CreateFirewallProfileGroup API operation for FortiOS creates a new Profile Group.
+// Returns the index value of the Profile Group and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the firewall - profile-group chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) CreateFirewallProfileGroup(params *map[string]interface{}) (output map[string]interface{}, err error) {
+
+	HTTPMethod := "POST"
+	path := "/api/v2/cmdb/firewall/profile-group"
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// UpdateFirewallProfileGroup API operation for FortiOS updates the specified Profile Group.
+// Returns the index value of the Profile Group and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the firewall - profile-group chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) UpdateFirewallProfileGroup(params *map[string]interface{}, mkey string) (output map[string]interface{}, err error) {
+	HTTPMethod := "PUT"
+	path := "/api/v2/cmdb/firewall/profile-group"
+	path += "/" + escapeURLString(mkey)
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// DeleteFirewallProfileGroup API operation for FortiOS deletes the specified Profile Group.
+// Returns error for service API and SDK errors.
+// See the firewall - profile-group chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) DeleteFirewallProfileGroup(mkey string) (err error) {
+	HTTPMethod := "DELETE"
+	path := "/api/v2/cmdb/firewall/profile-group"
+	path += "/" + escapeURLString(mkey)
+
+	err = delete(c, HTTPMethod, path)
+	return
+}
+
+// ReadFirewallProfileGroup API operation for FortiOS gets the Profile Group
+// with the specified index value.
+// Returns the requested Profile Group value when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the firewall - profile-group chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) ReadFirewallProfileGroup(mkey string) (mapTmp map[string]interface{}, err error) {
+	HTTPMethod := "GET"
+	path := "/api/v2/cmdb/firewall/profile-group"
+	path += "/" + escapeURLString(mkey)
+
+	mapTmp, err = read(c, HTTPMethod, path, false)
+	return
+}
+
