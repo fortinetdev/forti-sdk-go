1bf992eea18f9939cfbc35467d9762705fcc72c2
Frank Shen <fshen01@fortinet.com>
2020-08-22 18:15:30 +0800

    Support RouterSetting

--- fortios/sdkcore/sdkfos.go
+++ fortios/sdkcore/sdkfos.go
@@ -15244,3 +15244,58 @@ func (c *FortiSDKClient) ReadRouterMulticast6(mkey string) (mapTmp map[string]in
 	return
 }
 
+// CreateRouterAuthPath API operation for FortiOS creates a new Auth Path.
+// Returns the index value of the Auth Path and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the router - auth-path chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) CreateRouterAuthPath(params *map[string]interface{}) (output map[string]interface{}, err error) {
+
+	HTTPMethod := "POST"
+	path := "/api/v2/cmdb/router/auth-path"
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// UpdateRouterAuthPath API operation for FortiOS updates the specified Auth Path.
+// Returns the index value of the Auth Path and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the router - auth-path chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) UpdateRouterAuthPath(params *map[string]interface{}, mkey string) (output map[string]interface{}, err error) {
+	HTTPMethod := "PUT"
+	path := "/api/v2/cmdb/router/auth-path"
+	path += "/" + escapeURLString(mkey)
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// DeleteRouterAuthPath API operation for FortiOS deletes the specified Auth Path.
+// Returns error for service API and SDK errors.
+// See the router - auth-path chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) DeleteRouterAuthPath(mkey string) (err error) {
+	HTTPMethod := "DELETE"
+	path := "/api/v2/cmdb/router/auth-path"
+	path += "/" + escapeURLString(mkey)
+
+	err = delete(c, HTTPMethod, path)
+	return
+}
+
+// ReadRouterAuthPath API operation for FortiOS gets the Auth Path
+// with the specified index value.
+// Returns the requested Auth Path value when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the router - auth-path chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) ReadRouterAuthPath(mkey string) (mapTmp map[string]interface{}, err error) {
+	HTTPMethod := "GET"
+	path := "/api/v2/cmdb/router/auth-path"
+	path += "/" + escapeURLString(mkey)
+
+	mapTmp, err = read(c, HTTPMethod, path, false)
+	return
+}
+
+
