194c94f74145348241a43f8edecd8171d0bc5864
Frank Shen <fshen01@fortinet.com>
2020-08-22 17:57:24 +0800

    Support SpamfilterDnsbl

--- fortios/sdkcore/sdkfos.go
+++ fortios/sdkcore/sdkfos.go
@@ -10492,3 +10492,57 @@ func (c *FortiSDKClient) ReadSpamfilterBwl(mkey string) (mapTmp map[string]inter
 	return
 }
 
+// CreateSpamfilterMheader API operation for FortiOS creates a new Mheader.
+// Returns the index value of the Mheader and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the spamfilter - mheader chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) CreateSpamfilterMheader(params *map[string]interface{}) (output map[string]interface{}, err error) {
+
+	HTTPMethod := "POST"
+	path := "/api/v2/cmdb/spamfilter/mheader"
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// UpdateSpamfilterMheader API operation for FortiOS updates the specified Mheader.
+// Returns the index value of the Mheader and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the spamfilter - mheader chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) UpdateSpamfilterMheader(params *map[string]interface{}, mkey string) (output map[string]interface{}, err error) {
+	HTTPMethod := "PUT"
+	path := "/api/v2/cmdb/spamfilter/mheader"
+	path += "/" + escapeURLString(mkey)
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// DeleteSpamfilterMheader API operation for FortiOS deletes the specified Mheader.
+// Returns error for service API and SDK errors.
+// See the spamfilter - mheader chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) DeleteSpamfilterMheader(mkey string) (err error) {
+	HTTPMethod := "DELETE"
+	path := "/api/v2/cmdb/spamfilter/mheader"
+	path += "/" + escapeURLString(mkey)
+
+	err = delete(c, HTTPMethod, path)
+	return
+}
+
+// ReadSpamfilterMheader API operation for FortiOS gets the Mheader
+// with the specified index value.
+// Returns the requested Mheader value when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the spamfilter - mheader chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) ReadSpamfilterMheader(mkey string) (mapTmp map[string]interface{}, err error) {
+	HTTPMethod := "GET"
+	path := "/api/v2/cmdb/spamfilter/mheader"
+	path += "/" + escapeURLString(mkey)
+
+	mapTmp, err = read(c, HTTPMethod, path, false)
+	return
+}
+
