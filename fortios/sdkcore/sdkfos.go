f321c826ce7e4a810943441b566fe0f3e5522e07
Frank Shen <fshen01@fortinet.com>
2020-08-22 17:38:40 +0800

    Support ExtenderControllerExtender

--- fortios/sdkcore/sdkfos.go
+++ fortios/sdkcore/sdkfos.go
@@ -4805,3 +4805,38 @@ func (c *FortiSDKClient) ReadSwitchControllerSflow(mkey string) (mapTmp map[stri
 }
 
 
+// UpdateSwitchControllerNetworkMonitorSettings API operation for FortiOS updates the specified Network Monitor Settings.
+// Returns the index value of the Network Monitor Settings and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the switch-controller - network-monitor-settings chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) UpdateSwitchControllerNetworkMonitorSettings(params *map[string]interface{}, mkey string) (output map[string]interface{}, err error) {
+	HTTPMethod := "PUT"
+	path := "/api/v2/cmdb/switch-controller/network-monitor-settings"
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// DeleteSwitchControllerNetworkMonitorSettings API operation for FortiOS deletes the specified Network Monitor Settings.
+// Returns error for service API and SDK errors.
+// See the switch-controller - network-monitor-settings chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) DeleteSwitchControllerNetworkMonitorSettings(mkey string) (err error) {
+
+	//No unset API for switch-controller - network-monitor-settings
+	return
+}
+
+// ReadSwitchControllerNetworkMonitorSettings API operation for FortiOS gets the Network Monitor Settings
+// with the specified index value.
+// Returns the requested Network Monitor Settings value when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the switch-controller - network-monitor-settings chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) ReadSwitchControllerNetworkMonitorSettings(mkey string) (mapTmp map[string]interface{}, err error) {
+	HTTPMethod := "GET"
+	path := "/api/v2/cmdb/switch-controller/network-monitor-settings"
+
+	mapTmp, err = read(c, HTTPMethod, path, true)
+	return
+}
+
