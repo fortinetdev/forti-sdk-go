fc70e99d7a2cf805d6aee52728e1fcf7ec2434ee
Frank Shen <fshen01@fortinet.com>
2020-08-22 17:25:37 +0800

    Support SystemAutoupdatePushUpdate

--- fortios/sdkcore/sdkfos.go
+++ fortios/sdkcore/sdkfos.go
@@ -1150,3 +1150,58 @@ func (c *FortiSDKClient) ReadSystemSnmpCommunity(mkey string) (mapTmp map[string
 	return
 }
 
+// CreateSystemSnmpUser API operation for FortiOS creates a new User.
+// Returns the index value of the User and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the system.snmp - user chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) CreateSystemSnmpUser(params *map[string]interface{}) (output map[string]interface{}, err error) {
+
+	HTTPMethod := "POST"
+	path := "/api/v2/cmdb/system.snmp/user"
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// UpdateSystemSnmpUser API operation for FortiOS updates the specified User.
+// Returns the index value of the User and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the system.snmp - user chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) UpdateSystemSnmpUser(params *map[string]interface{}, mkey string) (output map[string]interface{}, err error) {
+	HTTPMethod := "PUT"
+	path := "/api/v2/cmdb/system.snmp/user"
+	path += "/" + escapeURLString(mkey)
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// DeleteSystemSnmpUser API operation for FortiOS deletes the specified User.
+// Returns error for service API and SDK errors.
+// See the system.snmp - user chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) DeleteSystemSnmpUser(mkey string) (err error) {
+	HTTPMethod := "DELETE"
+	path := "/api/v2/cmdb/system.snmp/user"
+	path += "/" + escapeURLString(mkey)
+
+	err = delete(c, HTTPMethod, path)
+	return
+}
+
+// ReadSystemSnmpUser API operation for FortiOS gets the User
+// with the specified index value.
+// Returns the requested User value when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the system.snmp - user chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) ReadSystemSnmpUser(mkey string) (mapTmp map[string]interface{}, err error) {
+	HTTPMethod := "GET"
+	path := "/api/v2/cmdb/system.snmp/user"
+	path += "/" + escapeURLString(mkey)
+
+	mapTmp, err = read(c, HTTPMethod, path, false)
+	return
+}
+
+
