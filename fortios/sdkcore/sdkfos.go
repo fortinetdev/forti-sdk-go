c4f9b8aefcafd857df106954bfbba06cbf9f9ceb
Frank Shen <fshen01@fortinet.com>
2020-08-22 17:25:17 +0800

    Support SystemSnmpCommunity

--- fortios/sdkcore/sdkfos.go
+++ fortios/sdkcore/sdkfos.go
@@ -1061,3 +1061,38 @@ func (c *FortiSDKClient) ReadSystemReplacemsgGroup(mkey string) (mapTmp map[stri
 }
 
 
+// UpdateSystemSnmpSysinfo API operation for FortiOS updates the specified Sysinfo.
+// Returns the index value of the Sysinfo and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the system.snmp - sysinfo chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) UpdateSystemSnmpSysinfo(params *map[string]interface{}, mkey string) (output map[string]interface{}, err error) {
+	HTTPMethod := "PUT"
+	path := "/api/v2/cmdb/system.snmp/sysinfo"
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// DeleteSystemSnmpSysinfo API operation for FortiOS deletes the specified Sysinfo.
+// Returns error for service API and SDK errors.
+// See the system.snmp - sysinfo chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) DeleteSystemSnmpSysinfo(mkey string) (err error) {
+
+	//No unset API for system.snmp - sysinfo
+	return
+}
+
+// ReadSystemSnmpSysinfo API operation for FortiOS gets the Sysinfo
+// with the specified index value.
+// Returns the requested Sysinfo value when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the system.snmp - sysinfo chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) ReadSystemSnmpSysinfo(mkey string) (mapTmp map[string]interface{}, err error) {
+	HTTPMethod := "GET"
+	path := "/api/v2/cmdb/system.snmp/sysinfo"
+
+	mapTmp, err = read(c, HTTPMethod, path, true)
+	return
+}
+
