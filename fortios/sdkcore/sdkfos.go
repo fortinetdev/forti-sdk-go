7a9eacdaaf0785f082fd3122b496e9f612479c88
Frank Shen <fshen01@fortinet.com>
2020-08-22 18:11:18 +0800

    Support EndpointControlSettings

--- fortios/sdkcore/sdkfos.go
+++ fortios/sdkcore/sdkfos.go
@@ -14093,3 +14093,39 @@ func (c *FortiSDKClient) ReadAuthenticationRule(mkey string) (mapTmp map[string]
 }
 
 
+// UpdateAuthenticationSetting API operation for FortiOS updates the specified Setting.
+// Returns the index value of the Setting and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the authentication - setting chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) UpdateAuthenticationSetting(params *map[string]interface{}, mkey string) (output map[string]interface{}, err error) {
+	HTTPMethod := "PUT"
+	path := "/api/v2/cmdb/authentication/setting"
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// DeleteAuthenticationSetting API operation for FortiOS deletes the specified Setting.
+// Returns error for service API and SDK errors.
+// See the authentication - setting chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) DeleteAuthenticationSetting(mkey string) (err error) {
+
+	//No unset API for authentication - setting
+	return
+}
+
+// ReadAuthenticationSetting API operation for FortiOS gets the Setting
+// with the specified index value.
+// Returns the requested Setting value when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the authentication - setting chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) ReadAuthenticationSetting(mkey string) (mapTmp map[string]interface{}, err error) {
+	HTTPMethod := "GET"
+	path := "/api/v2/cmdb/authentication/setting"
+
+	mapTmp, err = read(c, HTTPMethod, path, true)
+	return
+}
+
+
