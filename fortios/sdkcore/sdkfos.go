8f3e3ae07436df040088f076380b50af037e75e5
Frank Shen <fshen01@fortinet.com>
2020-08-22 17:42:00 +0800

    Support FirewallIppool

--- fortios/sdkcore/sdkfos.go
+++ fortios/sdkcore/sdkfos.go
@@ -5866,3 +5866,57 @@ func (c *FortiSDKClient) ReadFirewallScheduleRecurring(mkey string) (mapTmp map[
 	return
 }
 
+// CreateFirewallScheduleGroup API operation for FortiOS creates a new Group.
+// Returns the index value of the Group and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the firewall.schedule - group chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) CreateFirewallScheduleGroup(params *map[string]interface{}) (output map[string]interface{}, err error) {
+
+	HTTPMethod := "POST"
+	path := "/api/v2/cmdb/firewall.schedule/group"
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// UpdateFirewallScheduleGroup API operation for FortiOS updates the specified Group.
+// Returns the index value of the Group and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the firewall.schedule - group chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) UpdateFirewallScheduleGroup(params *map[string]interface{}, mkey string) (output map[string]interface{}, err error) {
+	HTTPMethod := "PUT"
+	path := "/api/v2/cmdb/firewall.schedule/group"
+	path += "/" + escapeURLString(mkey)
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// DeleteFirewallScheduleGroup API operation for FortiOS deletes the specified Group.
+// Returns error for service API and SDK errors.
+// See the firewall.schedule - group chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) DeleteFirewallScheduleGroup(mkey string) (err error) {
+	HTTPMethod := "DELETE"
+	path := "/api/v2/cmdb/firewall.schedule/group"
+	path += "/" + escapeURLString(mkey)
+
+	err = delete(c, HTTPMethod, path)
+	return
+}
+
+// ReadFirewallScheduleGroup API operation for FortiOS gets the Group
+// with the specified index value.
+// Returns the requested Group value when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the firewall.schedule - group chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) ReadFirewallScheduleGroup(mkey string) (mapTmp map[string]interface{}, err error) {
+	HTTPMethod := "GET"
+	path := "/api/v2/cmdb/firewall.schedule/group"
+	path += "/" + escapeURLString(mkey)
+
+	mapTmp, err = read(c, HTTPMethod, path, false)
+	return
+}
+
