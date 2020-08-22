090500c44d2fc37a82d52ce6cdd11ee67590fa76
Frank Shen <fshen01@fortinet.com>
2020-08-22 18:06:17 +0800

    Support UserDomainController

--- fortios/sdkcore/sdkfos.go
+++ fortios/sdkcore/sdkfos.go
@@ -12580,3 +12580,57 @@ func (c *FortiSDKClient) ReadUserLdap(mkey string) (mapTmp map[string]interface{
 	return
 }
 
+// CreateUserKrbKeytab API operation for FortiOS creates a new Krb Keytab.
+// Returns the index value of the Krb Keytab and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the user - krb-keytab chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) CreateUserKrbKeytab(params *map[string]interface{}) (output map[string]interface{}, err error) {
+
+	HTTPMethod := "POST"
+	path := "/api/v2/cmdb/user/krb-keytab"
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// UpdateUserKrbKeytab API operation for FortiOS updates the specified Krb Keytab.
+// Returns the index value of the Krb Keytab and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the user - krb-keytab chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) UpdateUserKrbKeytab(params *map[string]interface{}, mkey string) (output map[string]interface{}, err error) {
+	HTTPMethod := "PUT"
+	path := "/api/v2/cmdb/user/krb-keytab"
+	path += "/" + escapeURLString(mkey)
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// DeleteUserKrbKeytab API operation for FortiOS deletes the specified Krb Keytab.
+// Returns error for service API and SDK errors.
+// See the user - krb-keytab chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) DeleteUserKrbKeytab(mkey string) (err error) {
+	HTTPMethod := "DELETE"
+	path := "/api/v2/cmdb/user/krb-keytab"
+	path += "/" + escapeURLString(mkey)
+
+	err = delete(c, HTTPMethod, path)
+	return
+}
+
+// ReadUserKrbKeytab API operation for FortiOS gets the Krb Keytab
+// with the specified index value.
+// Returns the requested Krb Keytab value when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the user - krb-keytab chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) ReadUserKrbKeytab(mkey string) (mapTmp map[string]interface{}, err error) {
+	HTTPMethod := "GET"
+	path := "/api/v2/cmdb/user/krb-keytab"
+	path += "/" + escapeURLString(mkey)
+
+	mapTmp, err = read(c, HTTPMethod, path, false)
+	return
+}
+
