22fdad396fab6631de4cefad6d3f391e29a86e83
Frank Shen <fshen01@fortinet.com>
2020-08-22 18:02:56 +0800

    Support LogGuiDisplay

--- fortios/sdkcore/sdkfos.go
+++ fortios/sdkcore/sdkfos.go
@@ -11753,3 +11753,39 @@ func (c *FortiSDKClient) ReadLogNullDeviceFilter(mkey string) (mapTmp map[string
 }
 
 
+// UpdateLogSetting API operation for FortiOS updates the specified Setting.
+// Returns the index value of the Setting and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the log - setting chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) UpdateLogSetting(params *map[string]interface{}, mkey string) (output map[string]interface{}, err error) {
+	HTTPMethod := "PUT"
+	path := "/api/v2/cmdb/log/setting"
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// DeleteLogSetting API operation for FortiOS deletes the specified Setting.
+// Returns error for service API and SDK errors.
+// See the log - setting chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) DeleteLogSetting(mkey string) (err error) {
+
+	//No unset API for log - setting
+	return
+}
+
+// ReadLogSetting API operation for FortiOS gets the Setting
+// with the specified index value.
+// Returns the requested Setting value when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the log - setting chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) ReadLogSetting(mkey string) (mapTmp map[string]interface{}, err error) {
+	HTTPMethod := "GET"
+	path := "/api/v2/cmdb/log/setting"
+
+	mapTmp, err = read(c, HTTPMethod, path, true)
+	return
+}
+
+
