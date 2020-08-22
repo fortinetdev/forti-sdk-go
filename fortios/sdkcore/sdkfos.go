e4a3365d47b73f31c15157c769a1f90b58e1f728
Frank Shen <fshen01@fortinet.com>
2020-08-22 17:56:54 +0800

    Support SpamfilterBword

--- fortios/sdkcore/sdkfos.go
+++ fortios/sdkcore/sdkfos.go
@@ -10349,3 +10349,38 @@ func (c *FortiSDKClient) ReadDlpSensor(mkey string) (mapTmp map[string]interface
 }
 
 
+// UpdateDlpSettings API operation for FortiOS updates the specified Settings.
+// Returns the index value of the Settings and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the dlp - settings chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) UpdateDlpSettings(params *map[string]interface{}, mkey string) (output map[string]interface{}, err error) {
+	HTTPMethod := "PUT"
+	path := "/api/v2/cmdb/dlp/settings"
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// DeleteDlpSettings API operation for FortiOS deletes the specified Settings.
+// Returns error for service API and SDK errors.
+// See the dlp - settings chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) DeleteDlpSettings(mkey string) (err error) {
+
+	//No unset API for dlp - settings
+	return
+}
+
+// ReadDlpSettings API operation for FortiOS gets the Settings
+// with the specified index value.
+// Returns the requested Settings value when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the dlp - settings chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) ReadDlpSettings(mkey string) (mapTmp map[string]interface{}, err error) {
+	HTTPMethod := "GET"
+	path := "/api/v2/cmdb/dlp/settings"
+
+	mapTmp, err = read(c, HTTPMethod, path, true)
+	return
+}
+
