b2bd93bca61eedc0a55539224306d9b7981b1fab
Frank Shen <fshen01@fortinet.com>
2020-08-22 18:10:58 +0800

    Support AuthenticationRule

--- fortios/sdkcore/sdkfos.go
+++ fortios/sdkcore/sdkfos.go
@@ -13984,3 +13984,57 @@ func (c *FortiSDKClient) ReadWafProfile(mkey string) (mapTmp map[string]interfac
 	return
 }
 
+// CreateAuthenticationScheme API operation for FortiOS creates a new Scheme.
+// Returns the index value of the Scheme and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the authentication - scheme chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) CreateAuthenticationScheme(params *map[string]interface{}) (output map[string]interface{}, err error) {
+
+	HTTPMethod := "POST"
+	path := "/api/v2/cmdb/authentication/scheme"
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// UpdateAuthenticationScheme API operation for FortiOS updates the specified Scheme.
+// Returns the index value of the Scheme and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the authentication - scheme chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) UpdateAuthenticationScheme(params *map[string]interface{}, mkey string) (output map[string]interface{}, err error) {
+	HTTPMethod := "PUT"
+	path := "/api/v2/cmdb/authentication/scheme"
+	path += "/" + escapeURLString(mkey)
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// DeleteAuthenticationScheme API operation for FortiOS deletes the specified Scheme.
+// Returns error for service API and SDK errors.
+// See the authentication - scheme chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) DeleteAuthenticationScheme(mkey string) (err error) {
+	HTTPMethod := "DELETE"
+	path := "/api/v2/cmdb/authentication/scheme"
+	path += "/" + escapeURLString(mkey)
+
+	err = delete(c, HTTPMethod, path)
+	return
+}
+
+// ReadAuthenticationScheme API operation for FortiOS gets the Scheme
+// with the specified index value.
+// Returns the requested Scheme value when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the authentication - scheme chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) ReadAuthenticationScheme(mkey string) (mapTmp map[string]interface{}, err error) {
+	HTTPMethod := "GET"
+	path := "/api/v2/cmdb/authentication/scheme"
+	path += "/" + escapeURLString(mkey)
+
+	mapTmp, err = read(c, HTTPMethod, path, false)
+	return
+}
+
