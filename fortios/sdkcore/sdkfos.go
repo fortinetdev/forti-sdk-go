c32a17883004c3b66c5695eb5086eed4b4568d41
Frank Shen <fshen01@fortinet.com>
2020-08-22 18:07:07 +0800

    Support UserLocal

--- fortios/sdkcore/sdkfos.go
+++ fortios/sdkcore/sdkfos.go
@@ -12850,3 +12850,57 @@ func (c *FortiSDKClient) ReadUserAdgrp(mkey string) (mapTmp map[string]interface
 	return
 }
 
+// CreateUserPasswordPolicy API operation for FortiOS creates a new Password Policy.
+// Returns the index value of the Password Policy and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the user - password-policy chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) CreateUserPasswordPolicy(params *map[string]interface{}) (output map[string]interface{}, err error) {
+
+	HTTPMethod := "POST"
+	path := "/api/v2/cmdb/user/password-policy"
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// UpdateUserPasswordPolicy API operation for FortiOS updates the specified Password Policy.
+// Returns the index value of the Password Policy and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the user - password-policy chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) UpdateUserPasswordPolicy(params *map[string]interface{}, mkey string) (output map[string]interface{}, err error) {
+	HTTPMethod := "PUT"
+	path := "/api/v2/cmdb/user/password-policy"
+	path += "/" + escapeURLString(mkey)
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// DeleteUserPasswordPolicy API operation for FortiOS deletes the specified Password Policy.
+// Returns error for service API and SDK errors.
+// See the user - password-policy chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) DeleteUserPasswordPolicy(mkey string) (err error) {
+	HTTPMethod := "DELETE"
+	path := "/api/v2/cmdb/user/password-policy"
+	path += "/" + escapeURLString(mkey)
+
+	err = delete(c, HTTPMethod, path)
+	return
+}
+
+// ReadUserPasswordPolicy API operation for FortiOS gets the Password Policy
+// with the specified index value.
+// Returns the requested Password Policy value when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the user - password-policy chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) ReadUserPasswordPolicy(mkey string) (mapTmp map[string]interface{}, err error) {
+	HTTPMethod := "GET"
+	path := "/api/v2/cmdb/user/password-policy"
+	path += "/" + escapeURLString(mkey)
+
+	mapTmp, err = read(c, HTTPMethod, path, false)
+	return
+}
+
