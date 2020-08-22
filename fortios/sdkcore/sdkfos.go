beadef4666737f1814805bdc9cb6025da97f8658
Frank Shen <fshen01@fortinet.com>
2020-08-22 18:09:28 +0800

    Support AntivirusProfile

--- fortios/sdkcore/sdkfos.go
+++ fortios/sdkcore/sdkfos.go
@@ -13535,3 +13535,38 @@ func (c *FortiSDKClient) ReadAntivirusHeuristic(mkey string) (mapTmp map[string]
 }
 
 
+// UpdateAntivirusQuarantine API operation for FortiOS updates the specified Quarantine.
+// Returns the index value of the Quarantine and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the antivirus - quarantine chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) UpdateAntivirusQuarantine(params *map[string]interface{}, mkey string) (output map[string]interface{}, err error) {
+	HTTPMethod := "PUT"
+	path := "/api/v2/cmdb/antivirus/quarantine"
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// DeleteAntivirusQuarantine API operation for FortiOS deletes the specified Quarantine.
+// Returns error for service API and SDK errors.
+// See the antivirus - quarantine chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) DeleteAntivirusQuarantine(mkey string) (err error) {
+
+	//No unset API for antivirus - quarantine
+	return
+}
+
+// ReadAntivirusQuarantine API operation for FortiOS gets the Quarantine
+// with the specified index value.
+// Returns the requested Quarantine value when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the antivirus - quarantine chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) ReadAntivirusQuarantine(mkey string) (mapTmp map[string]interface{}, err error) {
+	HTTPMethod := "GET"
+	path := "/api/v2/cmdb/antivirus/quarantine"
+
+	mapTmp, err = read(c, HTTPMethod, path, true)
+	return
+}
+
