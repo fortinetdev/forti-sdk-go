5c3073f7161820f8d6143bd9a41e51cf32067210
Frank Shen <fshen01@fortinet.com>
2020-08-22 17:27:07 +0800

    Support SystemFm

--- fortios/sdkcore/sdkfos.go
+++ fortios/sdkcore/sdkfos.go
@@ -1547,3 +1547,39 @@ func (c *FortiSDKClient) ReadSystemManagementTunnel(mkey string) (mapTmp map[str
 }
 
 
+// UpdateSystemFortimanager API operation for FortiOS updates the specified Fortimanager.
+// Returns the index value of the Fortimanager and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the system - fortimanager chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) UpdateSystemFortimanager(params *map[string]interface{}, mkey string) (output map[string]interface{}, err error) {
+	HTTPMethod := "PUT"
+	path := "/api/v2/cmdb/system/fortimanager"
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// DeleteSystemFortimanager API operation for FortiOS deletes the specified Fortimanager.
+// Returns error for service API and SDK errors.
+// See the system - fortimanager chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) DeleteSystemFortimanager(mkey string) (err error) {
+
+	//No unset API for system - fortimanager
+	return
+}
+
+// ReadSystemFortimanager API operation for FortiOS gets the Fortimanager
+// with the specified index value.
+// Returns the requested Fortimanager value when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the system - fortimanager chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) ReadSystemFortimanager(mkey string) (mapTmp map[string]interface{}, err error) {
+	HTTPMethod := "GET"
+	path := "/api/v2/cmdb/system/fortimanager"
+
+	mapTmp, err = read(c, HTTPMethod, path, true)
+	return
+}
+
+
