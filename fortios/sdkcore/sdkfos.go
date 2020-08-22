b5930d68648e4b5917a6734f6053cf06824846e3
Frank Shen <fshen01@fortinet.com>
2020-08-22 18:11:28 +0800

    Support EndpointControlProfile

--- fortios/sdkcore/sdkfos.go
+++ fortios/sdkcore/sdkfos.go
@@ -14129,3 +14129,38 @@ func (c *FortiSDKClient) ReadAuthenticationSetting(mkey string) (mapTmp map[stri
 }
 
 
+// UpdateEndpointControlSettings API operation for FortiOS updates the specified Settings.
+// Returns the index value of the Settings and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the endpoint-control - settings chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) UpdateEndpointControlSettings(params *map[string]interface{}, mkey string) (output map[string]interface{}, err error) {
+	HTTPMethod := "PUT"
+	path := "/api/v2/cmdb/endpoint-control/settings"
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// DeleteEndpointControlSettings API operation for FortiOS deletes the specified Settings.
+// Returns error for service API and SDK errors.
+// See the endpoint-control - settings chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) DeleteEndpointControlSettings(mkey string) (err error) {
+
+	//No unset API for endpoint-control - settings
+	return
+}
+
+// ReadEndpointControlSettings API operation for FortiOS gets the Settings
+// with the specified index value.
+// Returns the requested Settings value when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the endpoint-control - settings chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) ReadEndpointControlSettings(mkey string) (mapTmp map[string]interface{}, err error) {
+	HTTPMethod := "GET"
+	path := "/api/v2/cmdb/endpoint-control/settings"
+
+	mapTmp, err = read(c, HTTPMethod, path, true)
+	return
+}
+
