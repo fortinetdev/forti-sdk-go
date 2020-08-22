d4398e3138063be1b2455a9707b5fce1ec3f343c
Frank Shen <fshen01@fortinet.com>
2020-08-22 17:23:16 +0800

    Support SystemHaMonitor

--- fortios/sdkcore/sdkfos.go
+++ fortios/sdkcore/sdkfos.go
@@ -539,3 +539,39 @@ func (c *FortiSDKClient) ReadSystemFssoPolling(mkey string) (mapTmp map[string]i
 }
 
 
+// UpdateSystemHa API operation for FortiOS updates the specified Ha.
+// Returns the index value of the Ha and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the system - ha chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) UpdateSystemHa(params *map[string]interface{}, mkey string) (output map[string]interface{}, err error) {
+	HTTPMethod := "PUT"
+	path := "/api/v2/cmdb/system/ha"
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// DeleteSystemHa API operation for FortiOS deletes the specified Ha.
+// Returns error for service API and SDK errors.
+// See the system - ha chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) DeleteSystemHa(mkey string) (err error) {
+
+	//No unset API for system - ha
+	return
+}
+
+// ReadSystemHa API operation for FortiOS gets the Ha
+// with the specified index value.
+// Returns the requested Ha value when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the system - ha chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) ReadSystemHa(mkey string) (mapTmp map[string]interface{}, err error) {
+	HTTPMethod := "GET"
+	path := "/api/v2/cmdb/system/ha"
+
+	mapTmp, err = read(c, HTTPMethod, path, true)
+	return
+}
+
+
