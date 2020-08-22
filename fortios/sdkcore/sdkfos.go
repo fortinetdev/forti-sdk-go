3f43b4592bd7fe6238770d8c901e355e251953a5
Frank Shen <fshen01@fortinet.com>
2020-08-22 18:11:59 +0800

    Support RouterAccessList

--- fortios/sdkcore/sdkfos.go
+++ fortios/sdkcore/sdkfos.go
@@ -14273,3 +14273,38 @@ func (c *FortiSDKClient) ReadEndpointControlForticlientRegistrationSync(mkey str
 }
 
 
+// UpdateAlertemailSetting API operation for FortiOS updates the specified Setting.
+// Returns the index value of the Setting and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the alertemail - setting chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) UpdateAlertemailSetting(params *map[string]interface{}, mkey string) (output map[string]interface{}, err error) {
+	HTTPMethod := "PUT"
+	path := "/api/v2/cmdb/alertemail/setting"
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// DeleteAlertemailSetting API operation for FortiOS deletes the specified Setting.
+// Returns error for service API and SDK errors.
+// See the alertemail - setting chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) DeleteAlertemailSetting(mkey string) (err error) {
+
+	//No unset API for alertemail - setting
+	return
+}
+
+// ReadAlertemailSetting API operation for FortiOS gets the Setting
+// with the specified index value.
+// Returns the requested Setting value when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the alertemail - setting chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) ReadAlertemailSetting(mkey string) (mapTmp map[string]interface{}, err error) {
+	HTTPMethod := "GET"
+	path := "/api/v2/cmdb/alertemail/setting"
+
+	mapTmp, err = read(c, HTTPMethod, path, true)
+	return
+}
+
