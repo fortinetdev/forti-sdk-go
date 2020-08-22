01a354756025e8b407f58caed12172e48cc72606
Frank Shen <fshen01@fortinet.com>
2020-08-22 18:08:58 +0800

    Support AntivirusSettings

--- fortios/sdkcore/sdkfos.go
+++ fortios/sdkcore/sdkfos.go
@@ -13408,3 +13408,58 @@ func (c *FortiSDKClient) ReadDnsfilterDomainFilter(mkey string) (mapTmp map[stri
 	return
 }
 
+// CreateDnsfilterProfile API operation for FortiOS creates a new Profile.
+// Returns the index value of the Profile and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the dnsfilter - profile chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) CreateDnsfilterProfile(params *map[string]interface{}) (output map[string]interface{}, err error) {
+
+	HTTPMethod := "POST"
+	path := "/api/v2/cmdb/dnsfilter/profile"
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// UpdateDnsfilterProfile API operation for FortiOS updates the specified Profile.
+// Returns the index value of the Profile and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the dnsfilter - profile chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) UpdateDnsfilterProfile(params *map[string]interface{}, mkey string) (output map[string]interface{}, err error) {
+	HTTPMethod := "PUT"
+	path := "/api/v2/cmdb/dnsfilter/profile"
+	path += "/" + escapeURLString(mkey)
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// DeleteDnsfilterProfile API operation for FortiOS deletes the specified Profile.
+// Returns error for service API and SDK errors.
+// See the dnsfilter - profile chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) DeleteDnsfilterProfile(mkey string) (err error) {
+	HTTPMethod := "DELETE"
+	path := "/api/v2/cmdb/dnsfilter/profile"
+	path += "/" + escapeURLString(mkey)
+
+	err = delete(c, HTTPMethod, path)
+	return
+}
+
+// ReadDnsfilterProfile API operation for FortiOS gets the Profile
+// with the specified index value.
+// Returns the requested Profile value when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the dnsfilter - profile chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) ReadDnsfilterProfile(mkey string) (mapTmp map[string]interface{}, err error) {
+	HTTPMethod := "GET"
+	path := "/api/v2/cmdb/dnsfilter/profile"
+	path += "/" + escapeURLString(mkey)
+
+	mapTmp, err = read(c, HTTPMethod, path, false)
+	return
+}
+
+
