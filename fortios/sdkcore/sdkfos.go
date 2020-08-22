e2ad8ec0fe740c139dea122927167982e8f7b976
Frank Shen <fshen01@fortinet.com>
2020-08-22 17:36:09 +0800

    Support SwitchController8021XSettings

--- fortios/sdkcore/sdkfos.go
+++ fortios/sdkcore/sdkfos.go
@@ -4102,3 +4102,58 @@ func (c *FortiSDKClient) ReadSwitchControllerTrafficPolicy(mkey string) (mapTmp 
 	return
 }
 
+// CreateSwitchControllerSwitchInterfaceTag API operation for FortiOS creates a new Switch Interface Tag.
+// Returns the index value of the Switch Interface Tag and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the switch-controller - switch-interface-tag chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) CreateSwitchControllerSwitchInterfaceTag(params *map[string]interface{}) (output map[string]interface{}, err error) {
+
+	HTTPMethod := "POST"
+	path := "/api/v2/cmdb/switch-controller/switch-interface-tag"
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// UpdateSwitchControllerSwitchInterfaceTag API operation for FortiOS updates the specified Switch Interface Tag.
+// Returns the index value of the Switch Interface Tag and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the switch-controller - switch-interface-tag chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) UpdateSwitchControllerSwitchInterfaceTag(params *map[string]interface{}, mkey string) (output map[string]interface{}, err error) {
+	HTTPMethod := "PUT"
+	path := "/api/v2/cmdb/switch-controller/switch-interface-tag"
+	path += "/" + escapeURLString(mkey)
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// DeleteSwitchControllerSwitchInterfaceTag API operation for FortiOS deletes the specified Switch Interface Tag.
+// Returns error for service API and SDK errors.
+// See the switch-controller - switch-interface-tag chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) DeleteSwitchControllerSwitchInterfaceTag(mkey string) (err error) {
+	HTTPMethod := "DELETE"
+	path := "/api/v2/cmdb/switch-controller/switch-interface-tag"
+	path += "/" + escapeURLString(mkey)
+
+	err = delete(c, HTTPMethod, path)
+	return
+}
+
+// ReadSwitchControllerSwitchInterfaceTag API operation for FortiOS gets the Switch Interface Tag
+// with the specified index value.
+// Returns the requested Switch Interface Tag value when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the switch-controller - switch-interface-tag chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) ReadSwitchControllerSwitchInterfaceTag(mkey string) (mapTmp map[string]interface{}, err error) {
+	HTTPMethod := "GET"
+	path := "/api/v2/cmdb/switch-controller/switch-interface-tag"
+	path += "/" + escapeURLString(mkey)
+
+	mapTmp, err = read(c, HTTPMethod, path, false)
+	return
+}
+
+
