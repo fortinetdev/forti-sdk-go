93c6e1d8a6d725a9a9ac8d66567f73cf4d448019
Frank Shen <fshen01@fortinet.com>
2020-08-22 17:40:10 +0800

    Support FirewallWildcardFqdnGroup

--- fortios/sdkcore/sdkfos.go
+++ fortios/sdkcore/sdkfos.go
@@ -5272,3 +5272,57 @@ func (c *FortiSDKClient) ReadFirewallAddrgrp6(mkey string) (mapTmp map[string]in
 	return
 }
 
+// CreateFirewallWildcardFqdnCustom API operation for FortiOS creates a new Custom.
+// Returns the index value of the Custom and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the firewall.wildcard-fqdn - custom chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) CreateFirewallWildcardFqdnCustom(params *map[string]interface{}) (output map[string]interface{}, err error) {
+
+	HTTPMethod := "POST"
+	path := "/api/v2/cmdb/firewall.wildcard-fqdn/custom"
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// UpdateFirewallWildcardFqdnCustom API operation for FortiOS updates the specified Custom.
+// Returns the index value of the Custom and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the firewall.wildcard-fqdn - custom chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) UpdateFirewallWildcardFqdnCustom(params *map[string]interface{}, mkey string) (output map[string]interface{}, err error) {
+	HTTPMethod := "PUT"
+	path := "/api/v2/cmdb/firewall.wildcard-fqdn/custom"
+	path += "/" + escapeURLString(mkey)
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// DeleteFirewallWildcardFqdnCustom API operation for FortiOS deletes the specified Custom.
+// Returns error for service API and SDK errors.
+// See the firewall.wildcard-fqdn - custom chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) DeleteFirewallWildcardFqdnCustom(mkey string) (err error) {
+	HTTPMethod := "DELETE"
+	path := "/api/v2/cmdb/firewall.wildcard-fqdn/custom"
+	path += "/" + escapeURLString(mkey)
+
+	err = delete(c, HTTPMethod, path)
+	return
+}
+
+// ReadFirewallWildcardFqdnCustom API operation for FortiOS gets the Custom
+// with the specified index value.
+// Returns the requested Custom value when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the firewall.wildcard-fqdn - custom chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) ReadFirewallWildcardFqdnCustom(mkey string) (mapTmp map[string]interface{}, err error) {
+	HTTPMethod := "GET"
+	path := "/api/v2/cmdb/firewall.wildcard-fqdn/custom"
+	path += "/" + escapeURLString(mkey)
+
+	mapTmp, err = read(c, HTTPMethod, path, false)
+	return
+}
+
