ad0cbb0c16d5875a17ee6f725697929cf922a58a
Frank Shen <fshen01@fortinet.com>
2020-08-22 17:39:10 +0800

    Support FirewallAddress6Template

--- fortios/sdkcore/sdkfos.go
+++ fortios/sdkcore/sdkfos.go
@@ -4948,3 +4948,57 @@ func (c *FortiSDKClient) ReadFirewallAddress(mkey string) (mapTmp map[string]int
 	return
 }
 
+// CreateFirewallMulticastAddress API operation for FortiOS creates a new Multicast Address.
+// Returns the index value of the Multicast Address and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the firewall - multicast-address chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) CreateFirewallMulticastAddress(params *map[string]interface{}) (output map[string]interface{}, err error) {
+
+	HTTPMethod := "POST"
+	path := "/api/v2/cmdb/firewall/multicast-address"
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// UpdateFirewallMulticastAddress API operation for FortiOS updates the specified Multicast Address.
+// Returns the index value of the Multicast Address and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the firewall - multicast-address chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) UpdateFirewallMulticastAddress(params *map[string]interface{}, mkey string) (output map[string]interface{}, err error) {
+	HTTPMethod := "PUT"
+	path := "/api/v2/cmdb/firewall/multicast-address"
+	path += "/" + escapeURLString(mkey)
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// DeleteFirewallMulticastAddress API operation for FortiOS deletes the specified Multicast Address.
+// Returns error for service API and SDK errors.
+// See the firewall - multicast-address chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) DeleteFirewallMulticastAddress(mkey string) (err error) {
+	HTTPMethod := "DELETE"
+	path := "/api/v2/cmdb/firewall/multicast-address"
+	path += "/" + escapeURLString(mkey)
+
+	err = delete(c, HTTPMethod, path)
+	return
+}
+
+// ReadFirewallMulticastAddress API operation for FortiOS gets the Multicast Address
+// with the specified index value.
+// Returns the requested Multicast Address value when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the firewall - multicast-address chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) ReadFirewallMulticastAddress(mkey string) (mapTmp map[string]interface{}, err error) {
+	HTTPMethod := "GET"
+	path := "/api/v2/cmdb/firewall/multicast-address"
+	path += "/" + escapeURLString(mkey)
+
+	mapTmp, err = read(c, HTTPMethod, path, false)
+	return
+}
+
