74f34f30fca759d7ede9fc49a54a68aa4de6fca2
Frank Shen <fshen01@fortinet.com>
2020-08-22 18:08:07 +0800

    Support UserDevice

--- fortios/sdkcore/sdkfos.go
+++ fortios/sdkcore/sdkfos.go
@@ -13138,3 +13138,57 @@ func (c *FortiSDKClient) ReadUserQuarantine(mkey string) (mapTmp map[string]inte
 	return
 }
 
+// CreateUserGroup API operation for FortiOS creates a new Group.
+// Returns the index value of the Group and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the user - group chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) CreateUserGroup(params *map[string]interface{}) (output map[string]interface{}, err error) {
+
+	HTTPMethod := "POST"
+	path := "/api/v2/cmdb/user/group"
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// UpdateUserGroup API operation for FortiOS updates the specified Group.
+// Returns the index value of the Group and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the user - group chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) UpdateUserGroup(params *map[string]interface{}, mkey string) (output map[string]interface{}, err error) {
+	HTTPMethod := "PUT"
+	path := "/api/v2/cmdb/user/group"
+	path += "/" + escapeURLString(mkey)
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// DeleteUserGroup API operation for FortiOS deletes the specified Group.
+// Returns error for service API and SDK errors.
+// See the user - group chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) DeleteUserGroup(mkey string) (err error) {
+	HTTPMethod := "DELETE"
+	path := "/api/v2/cmdb/user/group"
+	path += "/" + escapeURLString(mkey)
+
+	err = delete(c, HTTPMethod, path)
+	return
+}
+
+// ReadUserGroup API operation for FortiOS gets the Group
+// with the specified index value.
+// Returns the requested Group value when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the user - group chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) ReadUserGroup(mkey string) (mapTmp map[string]interface{}, err error) {
+	HTTPMethod := "GET"
+	path := "/api/v2/cmdb/user/group"
+	path += "/" + escapeURLString(mkey)
+
+	mapTmp, err = read(c, HTTPMethod, path, false)
+	return
+}
+
