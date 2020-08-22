16c5ddc84648d807e205e8492140a5d508624269
Frank Shen <fshen01@fortinet.com>
2020-08-22 17:31:18 +0800

    Support SystemDscpBasedPriority

--- fortios/sdkcore/sdkfos.go
+++ fortios/sdkcore/sdkfos.go
@@ -2734,3 +2734,57 @@ func (c *FortiSDKClient) ReadSystemFipsCc(mkey string) (mapTmp map[string]interf
 	return
 }
 
+// CreateSystemTosBasedPriority API operation for FortiOS creates a new Tos Based Priority.
+// Returns the index value of the Tos Based Priority and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the system - tos-based-priority chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) CreateSystemTosBasedPriority(params *map[string]interface{}) (output map[string]interface{}, err error) {
+
+	HTTPMethod := "POST"
+	path := "/api/v2/cmdb/system/tos-based-priority"
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// UpdateSystemTosBasedPriority API operation for FortiOS updates the specified Tos Based Priority.
+// Returns the index value of the Tos Based Priority and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the system - tos-based-priority chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) UpdateSystemTosBasedPriority(params *map[string]interface{}, mkey string) (output map[string]interface{}, err error) {
+	HTTPMethod := "PUT"
+	path := "/api/v2/cmdb/system/tos-based-priority"
+	path += "/" + escapeURLString(mkey)
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// DeleteSystemTosBasedPriority API operation for FortiOS deletes the specified Tos Based Priority.
+// Returns error for service API and SDK errors.
+// See the system - tos-based-priority chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) DeleteSystemTosBasedPriority(mkey string) (err error) {
+	HTTPMethod := "DELETE"
+	path := "/api/v2/cmdb/system/tos-based-priority"
+	path += "/" + escapeURLString(mkey)
+
+	err = delete(c, HTTPMethod, path)
+	return
+}
+
+// ReadSystemTosBasedPriority API operation for FortiOS gets the Tos Based Priority
+// with the specified index value.
+// Returns the requested Tos Based Priority value when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the system - tos-based-priority chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) ReadSystemTosBasedPriority(mkey string) (mapTmp map[string]interface{}, err error) {
+	HTTPMethod := "GET"
+	path := "/api/v2/cmdb/system/tos-based-priority"
+	path += "/" + escapeURLString(mkey)
+
+	mapTmp, err = read(c, HTTPMethod, path, false)
+	return
+}
+
