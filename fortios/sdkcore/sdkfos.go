3e6a521dc98cbc34da78e7839d067b88a5eb74db
Frank Shen <fshen01@fortinet.com>
2020-08-22 17:28:57 +0800

    Support SystemPppoeInterface

--- fortios/sdkcore/sdkfos.go
+++ fortios/sdkcore/sdkfos.go
@@ -2068,3 +2068,57 @@ func (c *FortiSDKClient) ReadSystemIpipTunnel(mkey string) (mapTmp map[string]in
 	return
 }
 
+// CreateSystemMobileTunnel API operation for FortiOS creates a new Mobile Tunnel.
+// Returns the index value of the Mobile Tunnel and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the system - mobile-tunnel chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) CreateSystemMobileTunnel(params *map[string]interface{}) (output map[string]interface{}, err error) {
+
+	HTTPMethod := "POST"
+	path := "/api/v2/cmdb/system/mobile-tunnel"
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// UpdateSystemMobileTunnel API operation for FortiOS updates the specified Mobile Tunnel.
+// Returns the index value of the Mobile Tunnel and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the system - mobile-tunnel chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) UpdateSystemMobileTunnel(params *map[string]interface{}, mkey string) (output map[string]interface{}, err error) {
+	HTTPMethod := "PUT"
+	path := "/api/v2/cmdb/system/mobile-tunnel"
+	path += "/" + escapeURLString(mkey)
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// DeleteSystemMobileTunnel API operation for FortiOS deletes the specified Mobile Tunnel.
+// Returns error for service API and SDK errors.
+// See the system - mobile-tunnel chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) DeleteSystemMobileTunnel(mkey string) (err error) {
+	HTTPMethod := "DELETE"
+	path := "/api/v2/cmdb/system/mobile-tunnel"
+	path += "/" + escapeURLString(mkey)
+
+	err = delete(c, HTTPMethod, path)
+	return
+}
+
+// ReadSystemMobileTunnel API operation for FortiOS gets the Mobile Tunnel
+// with the specified index value.
+// Returns the requested Mobile Tunnel value when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the system - mobile-tunnel chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) ReadSystemMobileTunnel(mkey string) (mapTmp map[string]interface{}, err error) {
+	HTTPMethod := "GET"
+	path := "/api/v2/cmdb/system/mobile-tunnel"
+	path += "/" + escapeURLString(mkey)
+
+	mapTmp, err = read(c, HTTPMethod, path, false)
+	return
+}
+
