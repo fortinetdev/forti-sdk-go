5427ca56983e653501aea730e3b59e828143fab7
Frank Shen <fshen01@fortinet.com>
2020-08-22 17:29:38 +0800

    Support SystemResourceLimits

--- fortios/sdkcore/sdkfos.go
+++ fortios/sdkcore/sdkfos.go
@@ -2284,3 +2284,58 @@ func (c *FortiSDKClient) ReadSystemDnsDatabase(mkey string) (mapTmp map[string]i
 	return
 }
 
+// CreateSystemDnsServer API operation for FortiOS creates a new Dns Server.
+// Returns the index value of the Dns Server and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the system - dns-server chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) CreateSystemDnsServer(params *map[string]interface{}) (output map[string]interface{}, err error) {
+
+	HTTPMethod := "POST"
+	path := "/api/v2/cmdb/system/dns-server"
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// UpdateSystemDnsServer API operation for FortiOS updates the specified Dns Server.
+// Returns the index value of the Dns Server and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the system - dns-server chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) UpdateSystemDnsServer(params *map[string]interface{}, mkey string) (output map[string]interface{}, err error) {
+	HTTPMethod := "PUT"
+	path := "/api/v2/cmdb/system/dns-server"
+	path += "/" + escapeURLString(mkey)
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// DeleteSystemDnsServer API operation for FortiOS deletes the specified Dns Server.
+// Returns error for service API and SDK errors.
+// See the system - dns-server chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) DeleteSystemDnsServer(mkey string) (err error) {
+	HTTPMethod := "DELETE"
+	path := "/api/v2/cmdb/system/dns-server"
+	path += "/" + escapeURLString(mkey)
+
+	err = delete(c, HTTPMethod, path)
+	return
+}
+
+// ReadSystemDnsServer API operation for FortiOS gets the Dns Server
+// with the specified index value.
+// Returns the requested Dns Server value when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the system - dns-server chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) ReadSystemDnsServer(mkey string) (mapTmp map[string]interface{}, err error) {
+	HTTPMethod := "GET"
+	path := "/api/v2/cmdb/system/dns-server"
+	path += "/" + escapeURLString(mkey)
+
+	mapTmp, err = read(c, HTTPMethod, path, false)
+	return
+}
+
+
