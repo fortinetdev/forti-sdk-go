92b7068ad52bde22177d96a09cf5d45450812e46
Frank Shen <fshen01@fortinet.com>
2020-08-22 17:41:50 +0800

    Support FirewallScheduleGroup

--- fortios/sdkcore/sdkfos.go
+++ fortios/sdkcore/sdkfos.go
@@ -5812,3 +5812,57 @@ func (c *FortiSDKClient) ReadFirewallScheduleOnetime(mkey string) (mapTmp map[st
 	return
 }
 
+// CreateFirewallScheduleRecurring API operation for FortiOS creates a new Recurring.
+// Returns the index value of the Recurring and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the firewall.schedule - recurring chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) CreateFirewallScheduleRecurring(params *map[string]interface{}) (output map[string]interface{}, err error) {
+
+	HTTPMethod := "POST"
+	path := "/api/v2/cmdb/firewall.schedule/recurring"
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// UpdateFirewallScheduleRecurring API operation for FortiOS updates the specified Recurring.
+// Returns the index value of the Recurring and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the firewall.schedule - recurring chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) UpdateFirewallScheduleRecurring(params *map[string]interface{}, mkey string) (output map[string]interface{}, err error) {
+	HTTPMethod := "PUT"
+	path := "/api/v2/cmdb/firewall.schedule/recurring"
+	path += "/" + escapeURLString(mkey)
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// DeleteFirewallScheduleRecurring API operation for FortiOS deletes the specified Recurring.
+// Returns error for service API and SDK errors.
+// See the firewall.schedule - recurring chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) DeleteFirewallScheduleRecurring(mkey string) (err error) {
+	HTTPMethod := "DELETE"
+	path := "/api/v2/cmdb/firewall.schedule/recurring"
+	path += "/" + escapeURLString(mkey)
+
+	err = delete(c, HTTPMethod, path)
+	return
+}
+
+// ReadFirewallScheduleRecurring API operation for FortiOS gets the Recurring
+// with the specified index value.
+// Returns the requested Recurring value when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the firewall.schedule - recurring chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) ReadFirewallScheduleRecurring(mkey string) (mapTmp map[string]interface{}, err error) {
+	HTTPMethod := "GET"
+	path := "/api/v2/cmdb/firewall.schedule/recurring"
+	path += "/" + escapeURLString(mkey)
+
+	mapTmp, err = read(c, HTTPMethod, path, false)
+	return
+}
+
