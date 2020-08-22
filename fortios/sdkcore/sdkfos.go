7ccd4df5ad03ab733ce226c2a142ea9680b75b0a
Frank Shen <fshen01@fortinet.com>
2020-08-22 18:05:27 +0800

    Support SshFilterProfile

--- fortios/sdkcore/sdkfos.go
+++ fortios/sdkcore/sdkfos.go
@@ -12310,3 +12310,57 @@ func (c *FortiSDKClient) ReadIcapServer(mkey string) (mapTmp map[string]interfac
 	return
 }
 
+// CreateIcapProfile API operation for FortiOS creates a new Profile.
+// Returns the index value of the Profile and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the icap - profile chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) CreateIcapProfile(params *map[string]interface{}) (output map[string]interface{}, err error) {
+
+	HTTPMethod := "POST"
+	path := "/api/v2/cmdb/icap/profile"
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// UpdateIcapProfile API operation for FortiOS updates the specified Profile.
+// Returns the index value of the Profile and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the icap - profile chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) UpdateIcapProfile(params *map[string]interface{}, mkey string) (output map[string]interface{}, err error) {
+	HTTPMethod := "PUT"
+	path := "/api/v2/cmdb/icap/profile"
+	path += "/" + escapeURLString(mkey)
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// DeleteIcapProfile API operation for FortiOS deletes the specified Profile.
+// Returns error for service API and SDK errors.
+// See the icap - profile chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) DeleteIcapProfile(mkey string) (err error) {
+	HTTPMethod := "DELETE"
+	path := "/api/v2/cmdb/icap/profile"
+	path += "/" + escapeURLString(mkey)
+
+	err = delete(c, HTTPMethod, path)
+	return
+}
+
+// ReadIcapProfile API operation for FortiOS gets the Profile
+// with the specified index value.
+// Returns the requested Profile value when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the icap - profile chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) ReadIcapProfile(mkey string) (mapTmp map[string]interface{}, err error) {
+	HTTPMethod := "GET"
+	path := "/api/v2/cmdb/icap/profile"
+	path += "/" + escapeURLString(mkey)
+
+	mapTmp, err = read(c, HTTPMethod, path, false)
+	return
+}
+
