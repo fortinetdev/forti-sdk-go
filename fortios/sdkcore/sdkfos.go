8101e9355de2829c2b32c7e056749e8ab3d40b3e
Frank Shen <fshen01@fortinet.com>
2020-08-22 17:27:47 +0800

    Support SystemIpv6Tunnel

--- fortios/sdkcore/sdkfos.go
+++ fortios/sdkcore/sdkfos.go
@@ -1708,3 +1708,57 @@ func (c *FortiSDKClient) ReadSystemZone(mkey string) (mapTmp map[string]interfac
 	return
 }
 
+// CreateSystemSdnConnector API operation for FortiOS creates a new Sdn Connector.
+// Returns the index value of the Sdn Connector and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the system - sdn-connector chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) CreateSystemSdnConnector(params *map[string]interface{}) (output map[string]interface{}, err error) {
+
+	HTTPMethod := "POST"
+	path := "/api/v2/cmdb/system/sdn-connector"
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// UpdateSystemSdnConnector API operation for FortiOS updates the specified Sdn Connector.
+// Returns the index value of the Sdn Connector and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the system - sdn-connector chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) UpdateSystemSdnConnector(params *map[string]interface{}, mkey string) (output map[string]interface{}, err error) {
+	HTTPMethod := "PUT"
+	path := "/api/v2/cmdb/system/sdn-connector"
+	path += "/" + escapeURLString(mkey)
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// DeleteSystemSdnConnector API operation for FortiOS deletes the specified Sdn Connector.
+// Returns error for service API and SDK errors.
+// See the system - sdn-connector chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) DeleteSystemSdnConnector(mkey string) (err error) {
+	HTTPMethod := "DELETE"
+	path := "/api/v2/cmdb/system/sdn-connector"
+	path += "/" + escapeURLString(mkey)
+
+	err = delete(c, HTTPMethod, path)
+	return
+}
+
+// ReadSystemSdnConnector API operation for FortiOS gets the Sdn Connector
+// with the specified index value.
+// Returns the requested Sdn Connector value when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the system - sdn-connector chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) ReadSystemSdnConnector(mkey string) (mapTmp map[string]interface{}, err error) {
+	HTTPMethod := "GET"
+	path := "/api/v2/cmdb/system/sdn-connector"
+	path += "/" + escapeURLString(mkey)
+
+	mapTmp, err = read(c, HTTPMethod, path, false)
+	return
+}
+
