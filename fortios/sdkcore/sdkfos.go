f5aa89e8c89e68a653ebe763e689c7bf6be141ed
Frank Shen <fshen01@fortinet.com>
2020-08-22 18:03:16 +0800

    Support LogFortianalyzerOverrideSetting

--- fortios/sdkcore/sdkfos.go
+++ fortios/sdkcore/sdkfos.go
@@ -11825,3 +11825,39 @@ func (c *FortiSDKClient) ReadLogGuiDisplay(mkey string) (mapTmp map[string]inter
 }
 
 
+// UpdateLogFortianalyzerSetting API operation for FortiOS updates the specified Setting.
+// Returns the index value of the Setting and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the log.fortianalyzer - setting chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) UpdateLogFortianalyzerSetting(params *map[string]interface{}, mkey string) (output map[string]interface{}, err error) {
+	HTTPMethod := "PUT"
+	path := "/api/v2/cmdb/log.fortianalyzer/setting"
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// DeleteLogFortianalyzerSetting API operation for FortiOS deletes the specified Setting.
+// Returns error for service API and SDK errors.
+// See the log.fortianalyzer - setting chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) DeleteLogFortianalyzerSetting(mkey string) (err error) {
+
+	//No unset API for log.fortianalyzer - setting
+	return
+}
+
+// ReadLogFortianalyzerSetting API operation for FortiOS gets the Setting
+// with the specified index value.
+// Returns the requested Setting value when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the log.fortianalyzer - setting chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) ReadLogFortianalyzerSetting(mkey string) (mapTmp map[string]interface{}, err error) {
+	HTTPMethod := "GET"
+	path := "/api/v2/cmdb/log.fortianalyzer/setting"
+
+	mapTmp, err = read(c, HTTPMethod, path, true)
+	return
+}
+
+
