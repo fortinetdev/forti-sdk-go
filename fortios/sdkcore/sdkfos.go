881efa79384bb53c9b15f74c7a73491b41d13be7
Frank Shen <fshen01@fortinet.com>
2020-08-22 17:52:43 +0800

    Support WebfilterSearchEngine

--- fortios/sdkcore/sdkfos.go
+++ fortios/sdkcore/sdkfos.go
@@ -9142,3 +9142,57 @@ func (c *FortiSDKClient) ReadWebfilterOverride(mkey string) (mapTmp map[string]i
 	return
 }
 
+// CreateWebfilterFtgdLocalRating API operation for FortiOS creates a new Ftgd Local Rating.
+// Returns the index value of the Ftgd Local Rating and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the webfilter - ftgd-local-rating chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) CreateWebfilterFtgdLocalRating(params *map[string]interface{}) (output map[string]interface{}, err error) {
+
+	HTTPMethod := "POST"
+	path := "/api/v2/cmdb/webfilter/ftgd-local-rating"
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// UpdateWebfilterFtgdLocalRating API operation for FortiOS updates the specified Ftgd Local Rating.
+// Returns the index value of the Ftgd Local Rating and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the webfilter - ftgd-local-rating chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) UpdateWebfilterFtgdLocalRating(params *map[string]interface{}, mkey string) (output map[string]interface{}, err error) {
+	HTTPMethod := "PUT"
+	path := "/api/v2/cmdb/webfilter/ftgd-local-rating"
+	path += "/" + escapeURLString(mkey)
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// DeleteWebfilterFtgdLocalRating API operation for FortiOS deletes the specified Ftgd Local Rating.
+// Returns error for service API and SDK errors.
+// See the webfilter - ftgd-local-rating chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) DeleteWebfilterFtgdLocalRating(mkey string) (err error) {
+	HTTPMethod := "DELETE"
+	path := "/api/v2/cmdb/webfilter/ftgd-local-rating"
+	path += "/" + escapeURLString(mkey)
+
+	err = delete(c, HTTPMethod, path)
+	return
+}
+
+// ReadWebfilterFtgdLocalRating API operation for FortiOS gets the Ftgd Local Rating
+// with the specified index value.
+// Returns the requested Ftgd Local Rating value when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the webfilter - ftgd-local-rating chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) ReadWebfilterFtgdLocalRating(mkey string) (mapTmp map[string]interface{}, err error) {
+	HTTPMethod := "GET"
+	path := "/api/v2/cmdb/webfilter/ftgd-local-rating"
+	path += "/" + escapeURLString(mkey)
+
+	mapTmp, err = read(c, HTTPMethod, path, false)
+	return
+}
+
