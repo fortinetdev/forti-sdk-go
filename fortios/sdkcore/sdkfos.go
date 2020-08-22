25376e9fd9e85a1bf26c2ec6996655b9e356e994
Frank Shen <fshen01@fortinet.com>
2020-08-22 17:48:32 +0800

    Support VpnSslWebRealm

--- fortios/sdkcore/sdkfos.go
+++ fortios/sdkcore/sdkfos.go
@@ -7901,3 +7901,38 @@ func (c *FortiSDKClient) ReadVpnCertificateOcspServer(mkey string) (mapTmp map[s
 }
 
 
+// UpdateVpnCertificateSetting API operation for FortiOS updates the specified Setting.
+// Returns the index value of the Setting and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the vpn.certificate - setting chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) UpdateVpnCertificateSetting(params *map[string]interface{}, mkey string) (output map[string]interface{}, err error) {
+	HTTPMethod := "PUT"
+	path := "/api/v2/cmdb/vpn.certificate/setting"
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// DeleteVpnCertificateSetting API operation for FortiOS deletes the specified Setting.
+// Returns error for service API and SDK errors.
+// See the vpn.certificate - setting chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) DeleteVpnCertificateSetting(mkey string) (err error) {
+
+	//No unset API for vpn.certificate - setting
+	return
+}
+
+// ReadVpnCertificateSetting API operation for FortiOS gets the Setting
+// with the specified index value.
+// Returns the requested Setting value when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the vpn.certificate - setting chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) ReadVpnCertificateSetting(mkey string) (mapTmp map[string]interface{}, err error) {
+	HTTPMethod := "GET"
+	path := "/api/v2/cmdb/vpn.certificate/setting"
+
+	mapTmp, err = read(c, HTTPMethod, path, true)
+	return
+}
+
