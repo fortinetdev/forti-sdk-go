bd9361dcc8cbe70e1f94928fc11d3658cea6e2fd
Frank Shen <fshen01@fortinet.com>
2020-08-22 17:45:01 +0800

    Support FirewallShapingPolicy

--- fortios/sdkcore/sdkfos.go
+++ fortios/sdkcore/sdkfos.go
@@ -6802,3 +6802,57 @@ func (c *FortiSDKClient) ReadFirewallAuthPortal(mkey string) (mapTmp map[string]
 	return
 }
 
+// CreateFirewallPolicy API operation for FortiOS creates a new Policy.
+// Returns the index value of the Policy and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the firewall - policy chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) CreateFirewallPolicy(params *map[string]interface{}) (output map[string]interface{}, err error) {
+
+	HTTPMethod := "POST"
+	path := "/api/v2/cmdb/firewall/policy"
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// UpdateFirewallPolicy API operation for FortiOS updates the specified Policy.
+// Returns the index value of the Policy and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the firewall - policy chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) UpdateFirewallPolicy(params *map[string]interface{}, mkey string) (output map[string]interface{}, err error) {
+	HTTPMethod := "PUT"
+	path := "/api/v2/cmdb/firewall/policy"
+	path += "/" + escapeURLString(mkey)
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// DeleteFirewallPolicy API operation for FortiOS deletes the specified Policy.
+// Returns error for service API and SDK errors.
+// See the firewall - policy chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) DeleteFirewallPolicy(mkey string) (err error) {
+	HTTPMethod := "DELETE"
+	path := "/api/v2/cmdb/firewall/policy"
+	path += "/" + escapeURLString(mkey)
+
+	err = delete(c, HTTPMethod, path)
+	return
+}
+
+// ReadFirewallPolicy API operation for FortiOS gets the Policy
+// with the specified index value.
+// Returns the requested Policy value when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the firewall - policy chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) ReadFirewallPolicy(mkey string) (mapTmp map[string]interface{}, err error) {
+	HTTPMethod := "GET"
+	path := "/api/v2/cmdb/firewall/policy"
+	path += "/" + escapeURLString(mkey)
+
+	mapTmp, err = read(c, HTTPMethod, path, false)
+	return
+}
+
