f749d10c1c87abaa4e11f7c8946095c54c2bec1a
Frank Shen <fshen01@fortinet.com>
2020-08-22 18:13:09 +0800

    Support RouterRip

--- fortios/sdkcore/sdkfos.go
+++ fortios/sdkcore/sdkfos.go
@@ -14632,3 +14632,58 @@ func (c *FortiSDKClient) ReadRouterCommunityList(mkey string) (mapTmp map[string
 	return
 }
 
+// CreateRouterRouteMap API operation for FortiOS creates a new Route Map.
+// Returns the index value of the Route Map and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the router - route-map chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) CreateRouterRouteMap(params *map[string]interface{}) (output map[string]interface{}, err error) {
+
+	HTTPMethod := "POST"
+	path := "/api/v2/cmdb/router/route-map"
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// UpdateRouterRouteMap API operation for FortiOS updates the specified Route Map.
+// Returns the index value of the Route Map and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the router - route-map chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) UpdateRouterRouteMap(params *map[string]interface{}, mkey string) (output map[string]interface{}, err error) {
+	HTTPMethod := "PUT"
+	path := "/api/v2/cmdb/router/route-map"
+	path += "/" + escapeURLString(mkey)
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// DeleteRouterRouteMap API operation for FortiOS deletes the specified Route Map.
+// Returns error for service API and SDK errors.
+// See the router - route-map chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) DeleteRouterRouteMap(mkey string) (err error) {
+	HTTPMethod := "DELETE"
+	path := "/api/v2/cmdb/router/route-map"
+	path += "/" + escapeURLString(mkey)
+
+	err = delete(c, HTTPMethod, path)
+	return
+}
+
+// ReadRouterRouteMap API operation for FortiOS gets the Route Map
+// with the specified index value.
+// Returns the requested Route Map value when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the router - route-map chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) ReadRouterRouteMap(mkey string) (mapTmp map[string]interface{}, err error) {
+	HTTPMethod := "GET"
+	path := "/api/v2/cmdb/router/route-map"
+	path += "/" + escapeURLString(mkey)
+
+	mapTmp, err = read(c, HTTPMethod, path, false)
+	return
+}
+
+
