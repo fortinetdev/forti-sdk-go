9cff6dd730daaf0ac4b8ef7e60a29022bccebaa4
Frank Shen <fshen01@fortinet.com>
2020-08-22 17:27:37 +0800

    Support SystemSdnConnector

--- fortios/sdkcore/sdkfos.go
+++ fortios/sdkcore/sdkfos.go
@@ -1654,3 +1654,57 @@ func (c *FortiSDKClient) ReadSystemCentralManagement(mkey string) (mapTmp map[st
 	return
 }
 
+// CreateSystemZone API operation for FortiOS creates a new Zone.
+// Returns the index value of the Zone and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the system - zone chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) CreateSystemZone(params *map[string]interface{}) (output map[string]interface{}, err error) {
+
+	HTTPMethod := "POST"
+	path := "/api/v2/cmdb/system/zone"
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// UpdateSystemZone API operation for FortiOS updates the specified Zone.
+// Returns the index value of the Zone and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the system - zone chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) UpdateSystemZone(params *map[string]interface{}, mkey string) (output map[string]interface{}, err error) {
+	HTTPMethod := "PUT"
+	path := "/api/v2/cmdb/system/zone"
+	path += "/" + escapeURLString(mkey)
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// DeleteSystemZone API operation for FortiOS deletes the specified Zone.
+// Returns error for service API and SDK errors.
+// See the system - zone chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) DeleteSystemZone(mkey string) (err error) {
+	HTTPMethod := "DELETE"
+	path := "/api/v2/cmdb/system/zone"
+	path += "/" + escapeURLString(mkey)
+
+	err = delete(c, HTTPMethod, path)
+	return
+}
+
+// ReadSystemZone API operation for FortiOS gets the Zone
+// with the specified index value.
+// Returns the requested Zone value when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the system - zone chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) ReadSystemZone(mkey string) (mapTmp map[string]interface{}, err error) {
+	HTTPMethod := "GET"
+	path := "/api/v2/cmdb/system/zone"
+	path += "/" + escapeURLString(mkey)
+
+	mapTmp, err = read(c, HTTPMethod, path, false)
+	return
+}
+
