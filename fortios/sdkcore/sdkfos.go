4c716b11f0d45d62ffa65cc9be979e26afc70587
Frank Shen <fshen01@fortinet.com>
2020-08-22 17:22:16 +0800

    Support SystemSmsServer

--- fortios/sdkcore/sdkfos.go
+++ fortios/sdkcore/sdkfos.go
@@ -251,3 +251,38 @@ func (c *FortiSDKClient) ReadSystemPasswordPolicy(mkey string) (mapTmp map[strin
 }
 
 
+// UpdateSystemPasswordPolicyGuestAdmin API operation for FortiOS updates the specified Password Policy Guest Admin.
+// Returns the index value of the Password Policy Guest Admin and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the system - password-policy-guest-admin chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) UpdateSystemPasswordPolicyGuestAdmin(params *map[string]interface{}, mkey string) (output map[string]interface{}, err error) {
+	HTTPMethod := "PUT"
+	path := "/api/v2/cmdb/system/password-policy-guest-admin"
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// DeleteSystemPasswordPolicyGuestAdmin API operation for FortiOS deletes the specified Password Policy Guest Admin.
+// Returns error for service API and SDK errors.
+// See the system - password-policy-guest-admin chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) DeleteSystemPasswordPolicyGuestAdmin(mkey string) (err error) {
+
+	//No unset API for system - password-policy-guest-admin
+	return
+}
+
+// ReadSystemPasswordPolicyGuestAdmin API operation for FortiOS gets the Password Policy Guest Admin
+// with the specified index value.
+// Returns the requested Password Policy Guest Admin value when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the system - password-policy-guest-admin chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) ReadSystemPasswordPolicyGuestAdmin(mkey string) (mapTmp map[string]interface{}, err error) {
+	HTTPMethod := "GET"
+	path := "/api/v2/cmdb/system/password-policy-guest-admin"
+
+	mapTmp, err = read(c, HTTPMethod, path, true)
+	return
+}
+
