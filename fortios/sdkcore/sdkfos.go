e0b2ca90d39853131077abac170a23f843574e65
Frank Shen <fshen01@fortinet.com>
2020-08-22 18:06:27 +0800

    Support UserPop3

--- fortios/sdkcore/sdkfos.go
+++ fortios/sdkcore/sdkfos.go
@@ -12634,3 +12634,57 @@ func (c *FortiSDKClient) ReadUserKrbKeytab(mkey string) (mapTmp map[string]inter
 	return
 }
 
+// CreateUserDomainController API operation for FortiOS creates a new Domain Controller.
+// Returns the index value of the Domain Controller and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the user - domain-controller chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) CreateUserDomainController(params *map[string]interface{}) (output map[string]interface{}, err error) {
+
+	HTTPMethod := "POST"
+	path := "/api/v2/cmdb/user/domain-controller"
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// UpdateUserDomainController API operation for FortiOS updates the specified Domain Controller.
+// Returns the index value of the Domain Controller and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the user - domain-controller chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) UpdateUserDomainController(params *map[string]interface{}, mkey string) (output map[string]interface{}, err error) {
+	HTTPMethod := "PUT"
+	path := "/api/v2/cmdb/user/domain-controller"
+	path += "/" + escapeURLString(mkey)
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// DeleteUserDomainController API operation for FortiOS deletes the specified Domain Controller.
+// Returns error for service API and SDK errors.
+// See the user - domain-controller chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) DeleteUserDomainController(mkey string) (err error) {
+	HTTPMethod := "DELETE"
+	path := "/api/v2/cmdb/user/domain-controller"
+	path += "/" + escapeURLString(mkey)
+
+	err = delete(c, HTTPMethod, path)
+	return
+}
+
+// ReadUserDomainController API operation for FortiOS gets the Domain Controller
+// with the specified index value.
+// Returns the requested Domain Controller value when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the user - domain-controller chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) ReadUserDomainController(mkey string) (mapTmp map[string]interface{}, err error) {
+	HTTPMethod := "GET"
+	path := "/api/v2/cmdb/user/domain-controller"
+	path += "/" + escapeURLString(mkey)
+
+	mapTmp, err = read(c, HTTPMethod, path, false)
+	return
+}
+
