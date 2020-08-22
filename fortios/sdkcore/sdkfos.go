57873dc6518d4836588eb3d8ead93a4e487736ab
Frank Shen <fshen01@fortinet.com>
2020-08-22 18:02:16 +0800

    Support LogFortiguardOverrideFilter

--- fortios/sdkcore/sdkfos.go
+++ fortios/sdkcore/sdkfos.go
@@ -11609,3 +11609,39 @@ func (c *FortiSDKClient) ReadLogFortiguardOverrideSetting(mkey string) (mapTmp m
 }
 
 
+// UpdateLogFortiguardFilter API operation for FortiOS updates the specified Filter.
+// Returns the index value of the Filter and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the log.fortiguard - filter chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) UpdateLogFortiguardFilter(params *map[string]interface{}, mkey string) (output map[string]interface{}, err error) {
+	HTTPMethod := "PUT"
+	path := "/api/v2/cmdb/log.fortiguard/filter"
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// DeleteLogFortiguardFilter API operation for FortiOS deletes the specified Filter.
+// Returns error for service API and SDK errors.
+// See the log.fortiguard - filter chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) DeleteLogFortiguardFilter(mkey string) (err error) {
+
+	//No unset API for log.fortiguard - filter
+	return
+}
+
+// ReadLogFortiguardFilter API operation for FortiOS gets the Filter
+// with the specified index value.
+// Returns the requested Filter value when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the log.fortiguard - filter chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) ReadLogFortiguardFilter(mkey string) (mapTmp map[string]interface{}, err error) {
+	HTTPMethod := "GET"
+	path := "/api/v2/cmdb/log.fortiguard/filter"
+
+	mapTmp, err = read(c, HTTPMethod, path, true)
+	return
+}
+
+
