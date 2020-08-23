9c2f7eeecfc8de8dc01c8353556652a025f5b289
Frank Shen <fshen01@fortinet.com>
2020-08-23 22:02:09 +0800

    Imporve log message output

--- fortios/sdkcore/sdkfos.go
+++ fortios/sdkcore/sdkfos.go
@@ -15299,3 +15299,37 @@ func (c *FortiSDKClient) ReadRouterAuthPath(mkey string) (mapTmp map[string]inte
 }
 
 
+// UpdateRouterSetting API operation for FortiOS updates the specified Setting.
+// Returns the index value of the Setting and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the router - setting chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) UpdateRouterSetting(params *map[string]interface{}, mkey string) (output map[string]interface{}, err error) {
+	HTTPMethod := "PUT"
+	path := "/api/v2/cmdb/router/setting"
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// DeleteRouterSetting API operation for FortiOS deletes the specified Setting.
+// Returns error for service API and SDK errors.
+// See the router - setting chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) DeleteRouterSetting(mkey string) (err error) {
+
+	//No unset API for router - setting
+	return
+}
+
+// ReadRouterSetting API operation for FortiOS gets the Setting
+// with the specified index value.
+// Returns the requested Setting value when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the router - setting chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) ReadRouterSetting(mkey string) (mapTmp map[string]interface{}, err error) {
+	HTTPMethod := "GET"
+	path := "/api/v2/cmdb/router/setting"
+
+	mapTmp, err = read(c, HTTPMethod, path, true)
+	return
+}
