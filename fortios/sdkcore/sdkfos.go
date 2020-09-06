2220cf15dc3f66a3053e07aa32e3836a3aaf9572
Frank Shen <fshen01@fortinet.com>
2020-09-07 01:16:20 +0800

    Support SystemDhcp6Server

--- fortios/sdkcore/sdkfos.go
+++ fortios/sdkcore/sdkfos.go
@@ -17170,3 +17170,57 @@ func (c *FortiSDKClient) DeleteSystemReplacemsgIcap(mkey string) (err error) {
 func (c *FortiSDKClient) ReadSystemReplacemsgIcap(mkey string) (mapTmp map[string]interface{}, err error) {
 	HTTPMethod := "GET"
 	path := "/api/v2/cmdb/system.replacemsg/icap"
+// CreateSystemDhcp6Server API operation for FortiOS creates a new Server.
+// Returns the index value of the Server and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the system.dhcp6 - server chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) CreateSystemDhcp6Server(params *map[string]interface{}) (output map[string]interface{}, err error) {
+
+	HTTPMethod := "POST"
+	path := "/api/v2/cmdb/system.dhcp6/server"
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// UpdateSystemDhcp6Server API operation for FortiOS updates the specified Server.
+// Returns the index value of the Server and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the system.dhcp6 - server chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) UpdateSystemDhcp6Server(params *map[string]interface{}, mkey string) (output map[string]interface{}, err error) {
+	HTTPMethod := "PUT"
+	path := "/api/v2/cmdb/system.dhcp6/server"
+	path += "/" + escapeURLString(mkey)
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// DeleteSystemDhcp6Server API operation for FortiOS deletes the specified Server.
+// Returns error for service API and SDK errors.
+// See the system.dhcp6 - server chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) DeleteSystemDhcp6Server(mkey string) (err error) {
+	HTTPMethod := "DELETE"
+	path := "/api/v2/cmdb/system.dhcp6/server"
+	path += "/" + escapeURLString(mkey)
+
+	err = delete(c, HTTPMethod, path)
+	return
+}
+
+// ReadSystemDhcp6Server API operation for FortiOS gets the Server
+// with the specified index value.
+// Returns the requested Server value when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the system.dhcp6 - server chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) ReadSystemDhcp6Server(mkey string) (mapTmp map[string]interface{}, err error) {
+	HTTPMethod := "GET"
+	path := "/api/v2/cmdb/system.dhcp6/server"
+	path += "/" + escapeURLString(mkey)
+
+	mapTmp, err = read(c, HTTPMethod, path, false)
+	return
+}
+
