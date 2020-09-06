6723c5ed58ccd0db66c9a6ba79a36eddeddbda1d
Frank Shen <fshen01@fortinet.com>
2020-09-07 01:19:09 +0800

    Support WirelessControllerBleProfile

--- fortios/sdkcore/sdkfos.go
+++ fortios/sdkcore/sdkfos.go
@@ -18232,3 +18232,57 @@ func (c *FortiSDKClient) ReadWirelessControllerWidsProfile(mkey string) (mapTmp 
 	return
 }
 
+// CreateWirelessControllerBleProfile API operation for FortiOS creates a new Ble Profile.
+// Returns the index value of the Ble Profile and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the wireless-controller - ble-profile chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) CreateWirelessControllerBleProfile(params *map[string]interface{}) (output map[string]interface{}, err error) {
+
+	HTTPMethod := "POST"
+	path := "/api/v2/cmdb/wireless-controller/ble-profile"
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// UpdateWirelessControllerBleProfile API operation for FortiOS updates the specified Ble Profile.
+// Returns the index value of the Ble Profile and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the wireless-controller - ble-profile chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) UpdateWirelessControllerBleProfile(params *map[string]interface{}, mkey string) (output map[string]interface{}, err error) {
+	HTTPMethod := "PUT"
+	path := "/api/v2/cmdb/wireless-controller/ble-profile"
+	path += "/" + escapeURLString(mkey)
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// DeleteWirelessControllerBleProfile API operation for FortiOS deletes the specified Ble Profile.
+// Returns error for service API and SDK errors.
+// See the wireless-controller - ble-profile chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) DeleteWirelessControllerBleProfile(mkey string) (err error) {
+	HTTPMethod := "DELETE"
+	path := "/api/v2/cmdb/wireless-controller/ble-profile"
+	path += "/" + escapeURLString(mkey)
+
+	err = delete(c, HTTPMethod, path)
+	return
+}
+
+// ReadWirelessControllerBleProfile API operation for FortiOS gets the Ble Profile
+// with the specified index value.
+// Returns the requested Ble Profile value when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the wireless-controller - ble-profile chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) ReadWirelessControllerBleProfile(mkey string) (mapTmp map[string]interface{}, err error) {
+	HTTPMethod := "GET"
+	path := "/api/v2/cmdb/wireless-controller/ble-profile"
+	path += "/" + escapeURLString(mkey)
+
+	mapTmp, err = read(c, HTTPMethod, path, false)
+	return
+}
+
