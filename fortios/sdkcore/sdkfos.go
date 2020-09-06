f1dfa39e7e3618f6d1473afdcdb811aa6bcad728
Frank Shen <fshen01@fortinet.com>
2020-09-07 01:26:08 +0800

    Support LogSyslogd2OverrideSetting

--- fortios/sdkcore/sdkfos.go
+++ fortios/sdkcore/sdkfos.go
@@ -20842,3 +20842,39 @@ func (c *FortiSDKClient) ReadLogSyslogdOverrideSetting(mkey string) (mapTmp map[
 }
 
 
+// UpdateLogSyslogd2OverrideSetting API operation for FortiOS updates the specified Override Setting.
+// Returns the index value of the Override Setting and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the log.syslogd2 - override-setting chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) UpdateLogSyslogd2OverrideSetting(params *map[string]interface{}, mkey string) (output map[string]interface{}, err error) {
+	HTTPMethod := "PUT"
+	path := "/api/v2/cmdb/log.syslogd2/override-setting"
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// DeleteLogSyslogd2OverrideSetting API operation for FortiOS deletes the specified Override Setting.
+// Returns error for service API and SDK errors.
+// See the log.syslogd2 - override-setting chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) DeleteLogSyslogd2OverrideSetting(mkey string) (err error) {
+
+	//No unset API for log.syslogd2 - override-setting
+	return
+}
+
+// ReadLogSyslogd2OverrideSetting API operation for FortiOS gets the Override Setting
+// with the specified index value.
+// Returns the requested Override Setting value when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the log.syslogd2 - override-setting chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) ReadLogSyslogd2OverrideSetting(mkey string) (mapTmp map[string]interface{}, err error) {
+	HTTPMethod := "GET"
+	path := "/api/v2/cmdb/log.syslogd2/override-setting"
+
+	mapTmp, err = read(c, HTTPMethod, path, true)
+	return
+}
+
+
