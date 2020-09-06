d59913eb70818d197b6b06644a937ee060e2f947
Frank Shen <fshen01@fortinet.com>
2020-09-07 01:19:17 +0800

    Support WirelessControllerWtpProfile

--- fortios/sdkcore/sdkfos.go
+++ fortios/sdkcore/sdkfos.go
@@ -18286,3 +18286,57 @@ func (c *FortiSDKClient) ReadWirelessControllerBleProfile(mkey string) (mapTmp m
 	return
 }
 
+// CreateWirelessControllerWtpProfile API operation for FortiOS creates a new Wtp Profile.
+// Returns the index value of the Wtp Profile and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the wireless-controller - wtp-profile chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) CreateWirelessControllerWtpProfile(params *map[string]interface{}) (output map[string]interface{}, err error) {
+
+	HTTPMethod := "POST"
+	path := "/api/v2/cmdb/wireless-controller/wtp-profile"
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// UpdateWirelessControllerWtpProfile API operation for FortiOS updates the specified Wtp Profile.
+// Returns the index value of the Wtp Profile and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the wireless-controller - wtp-profile chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) UpdateWirelessControllerWtpProfile(params *map[string]interface{}, mkey string) (output map[string]interface{}, err error) {
+	HTTPMethod := "PUT"
+	path := "/api/v2/cmdb/wireless-controller/wtp-profile"
+	path += "/" + escapeURLString(mkey)
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// DeleteWirelessControllerWtpProfile API operation for FortiOS deletes the specified Wtp Profile.
+// Returns error for service API and SDK errors.
+// See the wireless-controller - wtp-profile chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) DeleteWirelessControllerWtpProfile(mkey string) (err error) {
+	HTTPMethod := "DELETE"
+	path := "/api/v2/cmdb/wireless-controller/wtp-profile"
+	path += "/" + escapeURLString(mkey)
+
+	err = delete(c, HTTPMethod, path)
+	return
+}
+
+// ReadWirelessControllerWtpProfile API operation for FortiOS gets the Wtp Profile
+// with the specified index value.
+// Returns the requested Wtp Profile value when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the wireless-controller - wtp-profile chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) ReadWirelessControllerWtpProfile(mkey string) (mapTmp map[string]interface{}, err error) {
+	HTTPMethod := "GET"
+	path := "/api/v2/cmdb/wireless-controller/wtp-profile"
+	path += "/" + escapeURLString(mkey)
+
+	mapTmp, err = read(c, HTTPMethod, path, false)
+	return
+}
+
