da54f0431f38d47dd1c925101b2ebe6ecc816730
Frank Shen <fshen01@fortinet.com>
2020-09-07 01:16:52 +0800

    Support SystemPtp

--- fortios/sdkcore/sdkfos.go
+++ fortios/sdkcore/sdkfos.go
@@ -17369,3 +17369,38 @@ func (c *FortiSDKClient) ReadSystemMacAddressTable(mkey string) (mapTmp map[stri
 }
 
 
+// UpdateSystemPtp API operation for FortiOS updates the specified Ptp.
+// Returns the index value of the Ptp and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the system - ptp chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) UpdateSystemPtp(params *map[string]interface{}, mkey string) (output map[string]interface{}, err error) {
+	HTTPMethod := "PUT"
+	path := "/api/v2/cmdb/system/ptp"
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// DeleteSystemPtp API operation for FortiOS deletes the specified Ptp.
+// Returns error for service API and SDK errors.
+// See the system - ptp chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) DeleteSystemPtp(mkey string) (err error) {
+
+	//No unset API for system - ptp
+	return
+}
+
+// ReadSystemPtp API operation for FortiOS gets the Ptp
+// with the specified index value.
+// Returns the requested Ptp value when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the system - ptp chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) ReadSystemPtp(mkey string) (mapTmp map[string]interface{}, err error) {
+	HTTPMethod := "GET"
+	path := "/api/v2/cmdb/system/ptp"
+
+	mapTmp, err = read(c, HTTPMethod, path, true)
+	return
+}
+
