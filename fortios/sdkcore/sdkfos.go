e3d1870662bcefda6b44332d5971f80671768f2d
Frank Shen <fshen01@fortinet.com>
2020-09-07 01:17:48 +0800

    Support WirelessControllerHotspot20H2QpOsuProvider

--- fortios/sdkcore/sdkfos.go
+++ fortios/sdkcore/sdkfos.go
@@ -17728,3 +17728,57 @@ func (c *FortiSDKClient) ReadWirelessControllerHotspot20AnqpNaiRealm(mkey string
 	return
 }
 
+// CreateWirelessControllerHotspot20H2QpOsuProvider API operation for FortiOS creates a new H2Qp Osu Provider.
+// Returns the index value of the H2Qp Osu Provider and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the wireless-controller.hotspot20 - h2qp-osu-provider chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) CreateWirelessControllerHotspot20H2QpOsuProvider(params *map[string]interface{}) (output map[string]interface{}, err error) {
+
+	HTTPMethod := "POST"
+	path := "/api/v2/cmdb/wireless-controller.hotspot20/h2qp-osu-provider"
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// UpdateWirelessControllerHotspot20H2QpOsuProvider API operation for FortiOS updates the specified H2Qp Osu Provider.
+// Returns the index value of the H2Qp Osu Provider and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the wireless-controller.hotspot20 - h2qp-osu-provider chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) UpdateWirelessControllerHotspot20H2QpOsuProvider(params *map[string]interface{}, mkey string) (output map[string]interface{}, err error) {
+	HTTPMethod := "PUT"
+	path := "/api/v2/cmdb/wireless-controller.hotspot20/h2qp-osu-provider"
+	path += "/" + escapeURLString(mkey)
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// DeleteWirelessControllerHotspot20H2QpOsuProvider API operation for FortiOS deletes the specified H2Qp Osu Provider.
+// Returns error for service API and SDK errors.
+// See the wireless-controller.hotspot20 - h2qp-osu-provider chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) DeleteWirelessControllerHotspot20H2QpOsuProvider(mkey string) (err error) {
+	HTTPMethod := "DELETE"
+	path := "/api/v2/cmdb/wireless-controller.hotspot20/h2qp-osu-provider"
+	path += "/" + escapeURLString(mkey)
+
+	err = delete(c, HTTPMethod, path)
+	return
+}
+
+// ReadWirelessControllerHotspot20H2QpOsuProvider API operation for FortiOS gets the H2Qp Osu Provider
+// with the specified index value.
+// Returns the requested H2Qp Osu Provider value when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the wireless-controller.hotspot20 - h2qp-osu-provider chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) ReadWirelessControllerHotspot20H2QpOsuProvider(mkey string) (mapTmp map[string]interface{}, err error) {
+	HTTPMethod := "GET"
+	path := "/api/v2/cmdb/wireless-controller.hotspot20/h2qp-osu-provider"
+	path += "/" + escapeURLString(mkey)
+
+	mapTmp, err = read(c, HTTPMethod, path, false)
+	return
+}
+
