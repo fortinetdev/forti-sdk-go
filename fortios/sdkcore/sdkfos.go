217060160237fd6eedf26a816799461230ed6143
Frank Shen <fshen01@fortinet.com>
2020-09-07 01:24:47 +0800

    Support IpsDecoder

--- fortios/sdkcore/sdkfos.go
+++ fortios/sdkcore/sdkfos.go
@@ -20338,3 +20338,57 @@ func (c *FortiSDKClient) ReadIpsSensor(mkey string) (mapTmp map[string]interface
 	return
 }
 
+// CreateIpsDecoder API operation for FortiOS creates a new Decoder.
+// Returns the index value of the Decoder and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the ips - decoder chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) CreateIpsDecoder(params *map[string]interface{}) (output map[string]interface{}, err error) {
+
+	HTTPMethod := "POST"
+	path := "/api/v2/cmdb/ips/decoder"
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// UpdateIpsDecoder API operation for FortiOS updates the specified Decoder.
+// Returns the index value of the Decoder and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the ips - decoder chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) UpdateIpsDecoder(params *map[string]interface{}, mkey string) (output map[string]interface{}, err error) {
+	HTTPMethod := "PUT"
+	path := "/api/v2/cmdb/ips/decoder"
+	path += "/" + escapeURLString(mkey)
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// DeleteIpsDecoder API operation for FortiOS deletes the specified Decoder.
+// Returns error for service API and SDK errors.
+// See the ips - decoder chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) DeleteIpsDecoder(mkey string) (err error) {
+	HTTPMethod := "DELETE"
+	path := "/api/v2/cmdb/ips/decoder"
+	path += "/" + escapeURLString(mkey)
+
+	err = delete(c, HTTPMethod, path)
+	return
+}
+
+// ReadIpsDecoder API operation for FortiOS gets the Decoder
+// with the specified index value.
+// Returns the requested Decoder value when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the ips - decoder chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) ReadIpsDecoder(mkey string) (mapTmp map[string]interface{}, err error) {
+	HTTPMethod := "GET"
+	path := "/api/v2/cmdb/ips/decoder"
+	path += "/" + escapeURLString(mkey)
+
+	mapTmp, err = read(c, HTTPMethod, path, false)
+	return
+}
+
