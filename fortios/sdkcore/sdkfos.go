15ab472ff82290eaf6a23579abc7c0a7716ab336
Frank Shen <fshen01@fortinet.com>
2020-09-07 01:19:49 +0800

    Support WirelessControllerUtmProfile

--- fortios/sdkcore/sdkfos.go
+++ fortios/sdkcore/sdkfos.go
@@ -18502,3 +18502,57 @@ func (c *FortiSDKClient) ReadWirelessControllerQosProfile(mkey string) (mapTmp m
 	return
 }
 
+// CreateWirelessControllerUtmProfile API operation for FortiOS creates a new Utm Profile.
+// Returns the index value of the Utm Profile and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the wireless-controller - utm-profile chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) CreateWirelessControllerUtmProfile(params *map[string]interface{}) (output map[string]interface{}, err error) {
+
+	HTTPMethod := "POST"
+	path := "/api/v2/cmdb/wireless-controller/utm-profile"
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// UpdateWirelessControllerUtmProfile API operation for FortiOS updates the specified Utm Profile.
+// Returns the index value of the Utm Profile and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the wireless-controller - utm-profile chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) UpdateWirelessControllerUtmProfile(params *map[string]interface{}, mkey string) (output map[string]interface{}, err error) {
+	HTTPMethod := "PUT"
+	path := "/api/v2/cmdb/wireless-controller/utm-profile"
+	path += "/" + escapeURLString(mkey)
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// DeleteWirelessControllerUtmProfile API operation for FortiOS deletes the specified Utm Profile.
+// Returns error for service API and SDK errors.
+// See the wireless-controller - utm-profile chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) DeleteWirelessControllerUtmProfile(mkey string) (err error) {
+	HTTPMethod := "DELETE"
+	path := "/api/v2/cmdb/wireless-controller/utm-profile"
+	path += "/" + escapeURLString(mkey)
+
+	err = delete(c, HTTPMethod, path)
+	return
+}
+
+// ReadWirelessControllerUtmProfile API operation for FortiOS gets the Utm Profile
+// with the specified index value.
+// Returns the requested Utm Profile value when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the wireless-controller - utm-profile chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) ReadWirelessControllerUtmProfile(mkey string) (mapTmp map[string]interface{}, err error) {
+	HTTPMethod := "GET"
+	path := "/api/v2/cmdb/wireless-controller/utm-profile"
+	path += "/" + escapeURLString(mkey)
+
+	mapTmp, err = read(c, HTTPMethod, path, false)
+	return
+}
+
