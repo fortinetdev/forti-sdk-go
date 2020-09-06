c5cf5ff865165955032b5d76eb8fe6091c0f2677
Frank Shen <fshen01@fortinet.com>
2020-09-07 01:16:36 +0800

    Support SystemAlarm

--- fortios/sdkcore/sdkfos.go
+++ fortios/sdkcore/sdkfos.go
@@ -17279,3 +17279,38 @@ func (c *FortiSDKClient) ReadSystemVdomProperty(mkey string) (mapTmp map[string]
 }
 
 
+// UpdateSystemAlarm API operation for FortiOS updates the specified Alarm.
+// Returns the index value of the Alarm and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the system - alarm chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) UpdateSystemAlarm(params *map[string]interface{}, mkey string) (output map[string]interface{}, err error) {
+	HTTPMethod := "PUT"
+	path := "/api/v2/cmdb/system/alarm"
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// DeleteSystemAlarm API operation for FortiOS deletes the specified Alarm.
+// Returns error for service API and SDK errors.
+// See the system - alarm chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) DeleteSystemAlarm(mkey string) (err error) {
+
+	//No unset API for system - alarm
+	return
+}
+
+// ReadSystemAlarm API operation for FortiOS gets the Alarm
+// with the specified index value.
+// Returns the requested Alarm value when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the system - alarm chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) ReadSystemAlarm(mkey string) (mapTmp map[string]interface{}, err error) {
+	HTTPMethod := "GET"
+	path := "/api/v2/cmdb/system/alarm"
+
+	mapTmp, err = read(c, HTTPMethod, path, true)
+	return
+}
+
