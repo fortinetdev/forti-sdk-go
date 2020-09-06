7f1ef70c7672d614535927e0a5e80800ef343574
Frank Shen <fshen01@fortinet.com>
2020-09-07 01:14:51 +0800

    Support SystemReplacemsgSpam

--- fortios/sdkcore/sdkfos.go
+++ fortios/sdkcore/sdkfos.go
@@ -16582,3 +16582,57 @@ func (c *FortiSDKClient) ReadSystemReplacemsgFortiguardWf(mkey string) (mapTmp m
 	return
 }
 
+// CreateSystemReplacemsgSpam API operation for FortiOS creates a new Spam.
+// Returns the index value of the Spam and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the system.replacemsg - spam chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) CreateSystemReplacemsgSpam(params *map[string]interface{}) (output map[string]interface{}, err error) {
+
+	HTTPMethod := "POST"
+	path := "/api/v2/cmdb/system.replacemsg/spam"
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// UpdateSystemReplacemsgSpam API operation for FortiOS updates the specified Spam.
+// Returns the index value of the Spam and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the system.replacemsg - spam chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) UpdateSystemReplacemsgSpam(params *map[string]interface{}, mkey string) (output map[string]interface{}, err error) {
+	HTTPMethod := "PUT"
+	path := "/api/v2/cmdb/system.replacemsg/spam"
+	path += "/" + escapeURLString(mkey)
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// DeleteSystemReplacemsgSpam API operation for FortiOS deletes the specified Spam.
+// Returns error for service API and SDK errors.
+// See the system.replacemsg - spam chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) DeleteSystemReplacemsgSpam(mkey string) (err error) {
+	HTTPMethod := "DELETE"
+	path := "/api/v2/cmdb/system.replacemsg/spam"
+	path += "/" + escapeURLString(mkey)
+
+	err = delete(c, HTTPMethod, path)
+	return
+}
+
+// ReadSystemReplacemsgSpam API operation for FortiOS gets the Spam
+// with the specified index value.
+// Returns the requested Spam value when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the system.replacemsg - spam chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) ReadSystemReplacemsgSpam(mkey string) (mapTmp map[string]interface{}, err error) {
+	HTTPMethod := "GET"
+	path := "/api/v2/cmdb/system.replacemsg/spam"
+	path += "/" + escapeURLString(mkey)
+
+	mapTmp, err = read(c, HTTPMethod, path, false)
+	return
+}
+
