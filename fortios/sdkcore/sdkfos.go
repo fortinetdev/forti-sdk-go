cc90713952272e06997406c20e8280315d52330a
Frank Shen <fshen01@fortinet.com>
2020-09-07 01:15:15 +0800

    Support SystemReplacemsgAuth

--- fortios/sdkcore/sdkfos.go
+++ fortios/sdkcore/sdkfos.go
@@ -16744,3 +16744,57 @@ func (c *FortiSDKClient) ReadSystemReplacemsgAdmin(mkey string) (mapTmp map[stri
 	return
 }
 
+// CreateSystemReplacemsgAuth API operation for FortiOS creates a new Auth.
+// Returns the index value of the Auth and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the system.replacemsg - auth chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) CreateSystemReplacemsgAuth(params *map[string]interface{}) (output map[string]interface{}, err error) {
+
+	HTTPMethod := "POST"
+	path := "/api/v2/cmdb/system.replacemsg/auth"
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// UpdateSystemReplacemsgAuth API operation for FortiOS updates the specified Auth.
+// Returns the index value of the Auth and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the system.replacemsg - auth chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) UpdateSystemReplacemsgAuth(params *map[string]interface{}, mkey string) (output map[string]interface{}, err error) {
+	HTTPMethod := "PUT"
+	path := "/api/v2/cmdb/system.replacemsg/auth"
+	path += "/" + escapeURLString(mkey)
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// DeleteSystemReplacemsgAuth API operation for FortiOS deletes the specified Auth.
+// Returns error for service API and SDK errors.
+// See the system.replacemsg - auth chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) DeleteSystemReplacemsgAuth(mkey string) (err error) {
+	HTTPMethod := "DELETE"
+	path := "/api/v2/cmdb/system.replacemsg/auth"
+	path += "/" + escapeURLString(mkey)
+
+	err = delete(c, HTTPMethod, path)
+	return
+}
+
+// ReadSystemReplacemsgAuth API operation for FortiOS gets the Auth
+// with the specified index value.
+// Returns the requested Auth value when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the system.replacemsg - auth chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) ReadSystemReplacemsgAuth(mkey string) (mapTmp map[string]interface{}, err error) {
+	HTTPMethod := "GET"
+	path := "/api/v2/cmdb/system.replacemsg/auth"
+	path += "/" + escapeURLString(mkey)
+
+	mapTmp, err = read(c, HTTPMethod, path, false)
+	return
+}
+
