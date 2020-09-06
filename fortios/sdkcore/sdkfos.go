cbbccf98ab608fa1094a81ce2f8792e899b6706e
Frank Shen <fshen01@fortinet.com>
2020-09-07 01:17:08 +0800

    Support SystemGeoipOverride

--- fortios/sdkcore/sdkfos.go
+++ fortios/sdkcore/sdkfos.go
@@ -17458,3 +17458,60 @@ func (c *FortiSDKClient) ReadSystemVdomRadiusServer(mkey string) (mapTmp map[str
 	return
 }
 
+// CreateSystemGeoipOverride API operation for FortiOS creates a new Geoip Override.
+// Returns the index value of the Geoip Override and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the system - geoip-override chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) CreateSystemGeoipOverride(params *map[string]interface{}) (output map[string]interface{}, err error) {
+
+	HTTPMethod := "POST"
+	path := "/api/v2/cmdb/system/geoip-override"
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// UpdateSystemGeoipOverride API operation for FortiOS updates the specified Geoip Override.
+// Returns the index value of the Geoip Override and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the system - geoip-override chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) UpdateSystemGeoipOverride(params *map[string]interface{}, mkey string) (output map[string]interface{}, err error) {
+	HTTPMethod := "PUT"
+	path := "/api/v2/cmdb/system/geoip-override"
+	path += "/" + escapeURLString(mkey)
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// DeleteSystemGeoipOverride API operation for FortiOS deletes the specified Geoip Override.
+// Returns error for service API and SDK errors.
+// See the system - geoip-override chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) DeleteSystemGeoipOverride(mkey string) (err error) {
+	HTTPMethod := "DELETE"
+	path := "/api/v2/cmdb/system/geoip-override"
+	path += "/" + escapeURLString(mkey)
+
+	err = delete(c, HTTPMethod, path)
+	return
+}
+
+// ReadSystemGeoipOverride API operation for FortiOS gets the Geoip Override
+// with the specified index value.
+// Returns the requested Geoip Override value when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the system - geoip-override chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) ReadSystemGeoipOverride(mkey string) (mapTmp map[string]interface{}, err error) {
+	HTTPMethod := "GET"
+	path := "/api/v2/cmdb/system/geoip-override"
+	path += "/" + escapeURLString(mkey)
+
+	mapTmp, err = read(c, HTTPMethod, path, false)
+	return
+}
+
+	return
+}
+
