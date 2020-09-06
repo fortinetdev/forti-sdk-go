abf949e3610791d0aa017af6742f11cee6c449ab
Frank Shen <fshen01@fortinet.com>
2020-09-07 01:19:33 +0800

    Support WirelessControllerWtpGroup

--- fortios/sdkcore/sdkfos.go
+++ fortios/sdkcore/sdkfos.go
@@ -18394,3 +18394,57 @@ func (c *FortiSDKClient) ReadWirelessControllerWtp(mkey string) (mapTmp map[stri
 	return
 }
 
+// CreateWirelessControllerWtpGroup API operation for FortiOS creates a new Wtp Group.
+// Returns the index value of the Wtp Group and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the wireless-controller - wtp-group chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) CreateWirelessControllerWtpGroup(params *map[string]interface{}) (output map[string]interface{}, err error) {
+
+	HTTPMethod := "POST"
+	path := "/api/v2/cmdb/wireless-controller/wtp-group"
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// UpdateWirelessControllerWtpGroup API operation for FortiOS updates the specified Wtp Group.
+// Returns the index value of the Wtp Group and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the wireless-controller - wtp-group chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) UpdateWirelessControllerWtpGroup(params *map[string]interface{}, mkey string) (output map[string]interface{}, err error) {
+	HTTPMethod := "PUT"
+	path := "/api/v2/cmdb/wireless-controller/wtp-group"
+	path += "/" + escapeURLString(mkey)
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// DeleteWirelessControllerWtpGroup API operation for FortiOS deletes the specified Wtp Group.
+// Returns error for service API and SDK errors.
+// See the wireless-controller - wtp-group chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) DeleteWirelessControllerWtpGroup(mkey string) (err error) {
+	HTTPMethod := "DELETE"
+	path := "/api/v2/cmdb/wireless-controller/wtp-group"
+	path += "/" + escapeURLString(mkey)
+
+	err = delete(c, HTTPMethod, path)
+	return
+}
+
+// ReadWirelessControllerWtpGroup API operation for FortiOS gets the Wtp Group
+// with the specified index value.
+// Returns the requested Wtp Group value when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the wireless-controller - wtp-group chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) ReadWirelessControllerWtpGroup(mkey string) (mapTmp map[string]interface{}, err error) {
+	HTTPMethod := "GET"
+	path := "/api/v2/cmdb/wireless-controller/wtp-group"
+	path += "/" + escapeURLString(mkey)
+
+	mapTmp, err = read(c, HTTPMethod, path, false)
+	return
+}
+
