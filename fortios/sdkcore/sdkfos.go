1abee8b6e3ffd53257bee8226e5f2f204e47d3c6
Frank Shen <fshen01@fortinet.com>
2020-09-07 01:22:55 +0800

    Support FirewallPolicy46

--- fortios/sdkcore/sdkfos.go
+++ fortios/sdkcore/sdkfos.go
@@ -19636,3 +19636,57 @@ func (c *FortiSDKClient) ReadFirewallConsolidatedPolicy(mkey string) (mapTmp map
 	return
 }
 
+// CreateFirewallPolicy46 API operation for FortiOS creates a new Policy46.
+// Returns the index value of the Policy46 and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the firewall - policy46 chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) CreateFirewallPolicy46(params *map[string]interface{}) (output map[string]interface{}, err error) {
+
+	HTTPMethod := "POST"
+	path := "/api/v2/cmdb/firewall/policy46"
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// UpdateFirewallPolicy46 API operation for FortiOS updates the specified Policy46.
+// Returns the index value of the Policy46 and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the firewall - policy46 chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) UpdateFirewallPolicy46(params *map[string]interface{}, mkey string) (output map[string]interface{}, err error) {
+	HTTPMethod := "PUT"
+	path := "/api/v2/cmdb/firewall/policy46"
+	path += "/" + escapeURLString(mkey)
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// DeleteFirewallPolicy46 API operation for FortiOS deletes the specified Policy46.
+// Returns error for service API and SDK errors.
+// See the firewall - policy46 chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) DeleteFirewallPolicy46(mkey string) (err error) {
+	HTTPMethod := "DELETE"
+	path := "/api/v2/cmdb/firewall/policy46"
+	path += "/" + escapeURLString(mkey)
+
+	err = delete(c, HTTPMethod, path)
+	return
+}
+
+// ReadFirewallPolicy46 API operation for FortiOS gets the Policy46
+// with the specified index value.
+// Returns the requested Policy46 value when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the firewall - policy46 chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) ReadFirewallPolicy46(mkey string) (mapTmp map[string]interface{}, err error) {
+	HTTPMethod := "GET"
+	path := "/api/v2/cmdb/firewall/policy46"
+	path += "/" + escapeURLString(mkey)
+
+	mapTmp, err = read(c, HTTPMethod, path, false)
+	return
+}
+
