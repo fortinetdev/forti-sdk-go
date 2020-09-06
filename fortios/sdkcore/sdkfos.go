824fcf04a381a9ac0157a17ea975437ddc103bfe
Frank Shen <fshen01@fortinet.com>
2020-09-07 01:20:54 +0800

    Support SwitchControllerManagedSwitch

--- fortios/sdkcore/sdkfos.go
+++ fortios/sdkcore/sdkfos.go
@@ -18916,3 +18916,57 @@ func (c *FortiSDKClient) ReadSwitchControllerAutoConfigCustom(mkey string) (mapT
 	return
 }
 
+// CreateSwitchControllerManagedSwitch API operation for FortiOS creates a new Managed Switch.
+// Returns the index value of the Managed Switch and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the switch-controller - managed-switch chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) CreateSwitchControllerManagedSwitch(params *map[string]interface{}) (output map[string]interface{}, err error) {
+
+	HTTPMethod := "POST"
+	path := "/api/v2/cmdb/switch-controller/managed-switch"
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// UpdateSwitchControllerManagedSwitch API operation for FortiOS updates the specified Managed Switch.
+// Returns the index value of the Managed Switch and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the switch-controller - managed-switch chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) UpdateSwitchControllerManagedSwitch(params *map[string]interface{}, mkey string) (output map[string]interface{}, err error) {
+	HTTPMethod := "PUT"
+	path := "/api/v2/cmdb/switch-controller/managed-switch"
+	path += "/" + escapeURLString(mkey)
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// DeleteSwitchControllerManagedSwitch API operation for FortiOS deletes the specified Managed Switch.
+// Returns error for service API and SDK errors.
+// See the switch-controller - managed-switch chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) DeleteSwitchControllerManagedSwitch(mkey string) (err error) {
+	HTTPMethod := "DELETE"
+	path := "/api/v2/cmdb/switch-controller/managed-switch"
+	path += "/" + escapeURLString(mkey)
+
+	err = delete(c, HTTPMethod, path)
+	return
+}
+
+// ReadSwitchControllerManagedSwitch API operation for FortiOS gets the Managed Switch
+// with the specified index value.
+// Returns the requested Managed Switch value when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the switch-controller - managed-switch chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) ReadSwitchControllerManagedSwitch(mkey string) (mapTmp map[string]interface{}, err error) {
+	HTTPMethod := "GET"
+	path := "/api/v2/cmdb/switch-controller/managed-switch"
+	path += "/" + escapeURLString(mkey)
+
+	mapTmp, err = read(c, HTTPMethod, path, false)
+	return
+}
+
