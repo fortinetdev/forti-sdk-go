7b88c68a5913729693549e6031f4c075a6682992
Frank Shen <fshen01@fortinet.com>
2020-09-07 01:24:39 +0800

    Support IpsSensor

--- fortios/sdkcore/sdkfos.go
+++ fortios/sdkcore/sdkfos.go
@@ -20284,3 +20284,57 @@ func (c *FortiSDKClient) ReadCertificateCrl(mkey string) (mapTmp map[string]inte
 	return
 }
 
+// CreateIpsSensor API operation for FortiOS creates a new Sensor.
+// Returns the index value of the Sensor and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the ips - sensor chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) CreateIpsSensor(params *map[string]interface{}) (output map[string]interface{}, err error) {
+
+	HTTPMethod := "POST"
+	path := "/api/v2/cmdb/ips/sensor"
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// UpdateIpsSensor API operation for FortiOS updates the specified Sensor.
+// Returns the index value of the Sensor and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the ips - sensor chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) UpdateIpsSensor(params *map[string]interface{}, mkey string) (output map[string]interface{}, err error) {
+	HTTPMethod := "PUT"
+	path := "/api/v2/cmdb/ips/sensor"
+	path += "/" + escapeURLString(mkey)
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// DeleteIpsSensor API operation for FortiOS deletes the specified Sensor.
+// Returns error for service API and SDK errors.
+// See the ips - sensor chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) DeleteIpsSensor(mkey string) (err error) {
+	HTTPMethod := "DELETE"
+	path := "/api/v2/cmdb/ips/sensor"
+	path += "/" + escapeURLString(mkey)
+
+	err = delete(c, HTTPMethod, path)
+	return
+}
+
+// ReadIpsSensor API operation for FortiOS gets the Sensor
+// with the specified index value.
+// Returns the requested Sensor value when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the ips - sensor chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) ReadIpsSensor(mkey string) (mapTmp map[string]interface{}, err error) {
+	HTTPMethod := "GET"
+	path := "/api/v2/cmdb/ips/sensor"
+	path += "/" + escapeURLString(mkey)
+
+	mapTmp, err = read(c, HTTPMethod, path, false)
+	return
+}
+
