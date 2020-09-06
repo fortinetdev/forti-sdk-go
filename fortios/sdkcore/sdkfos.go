50e4621cd5d75cd3f1e095b694f5dc935c60d7ee
Frank Shen <fshen01@fortinet.com>
2020-09-07 01:27:04 +0800

    Support WafMainClass

--- fortios/sdkcore/sdkfos.go
+++ fortios/sdkcore/sdkfos.go
@@ -21166,3 +21166,57 @@ func (c *FortiSDKClient) ReadUserSecurityExemptList(mkey string) (mapTmp map[str
 	return
 }
 
+// CreateWafMainClass API operation for FortiOS creates a new Main Class.
+// Returns the index value of the Main Class and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the waf - main-class chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) CreateWafMainClass(params *map[string]interface{}) (output map[string]interface{}, err error) {
+
+	HTTPMethod := "POST"
+	path := "/api/v2/cmdb/waf/main-class"
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// UpdateWafMainClass API operation for FortiOS updates the specified Main Class.
+// Returns the index value of the Main Class and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the waf - main-class chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) UpdateWafMainClass(params *map[string]interface{}, mkey string) (output map[string]interface{}, err error) {
+	HTTPMethod := "PUT"
+	path := "/api/v2/cmdb/waf/main-class"
+	path += "/" + escapeURLString(mkey)
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// DeleteWafMainClass API operation for FortiOS deletes the specified Main Class.
+// Returns error for service API and SDK errors.
+// See the waf - main-class chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) DeleteWafMainClass(mkey string) (err error) {
+	HTTPMethod := "DELETE"
+	path := "/api/v2/cmdb/waf/main-class"
+	path += "/" + escapeURLString(mkey)
+
+	err = delete(c, HTTPMethod, path)
+	return
+}
+
+// ReadWafMainClass API operation for FortiOS gets the Main Class
+// with the specified index value.
+// Returns the requested Main Class value when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the waf - main-class chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) ReadWafMainClass(mkey string) (mapTmp map[string]interface{}, err error) {
+	HTTPMethod := "GET"
+	path := "/api/v2/cmdb/waf/main-class"
+	path += "/" + escapeURLString(mkey)
+
+	mapTmp, err = read(c, HTTPMethod, path, false)
+	return
+}
+
