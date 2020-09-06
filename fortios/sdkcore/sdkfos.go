a479f93aa9b5f56c92e6dd1661941fc6c0234551
Frank Shen <fshen01@fortinet.com>
2020-09-07 01:14:19 +0800

    Support SystemReplacemsgWebproxy

--- fortios/sdkcore/sdkfos.go
+++ fortios/sdkcore/sdkfos.go
@@ -16366,3 +16366,57 @@ func (c *FortiSDKClient) ReadSystemReplacemsgHttp(mkey string) (mapTmp map[strin
 	return
 }
 
+// CreateSystemReplacemsgWebproxy API operation for FortiOS creates a new Webproxy.
+// Returns the index value of the Webproxy and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the system.replacemsg - webproxy chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) CreateSystemReplacemsgWebproxy(params *map[string]interface{}) (output map[string]interface{}, err error) {
+
+	HTTPMethod := "POST"
+	path := "/api/v2/cmdb/system.replacemsg/webproxy"
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// UpdateSystemReplacemsgWebproxy API operation for FortiOS updates the specified Webproxy.
+// Returns the index value of the Webproxy and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the system.replacemsg - webproxy chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) UpdateSystemReplacemsgWebproxy(params *map[string]interface{}, mkey string) (output map[string]interface{}, err error) {
+	HTTPMethod := "PUT"
+	path := "/api/v2/cmdb/system.replacemsg/webproxy"
+	path += "/" + escapeURLString(mkey)
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// DeleteSystemReplacemsgWebproxy API operation for FortiOS deletes the specified Webproxy.
+// Returns error for service API and SDK errors.
+// See the system.replacemsg - webproxy chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) DeleteSystemReplacemsgWebproxy(mkey string) (err error) {
+	HTTPMethod := "DELETE"
+	path := "/api/v2/cmdb/system.replacemsg/webproxy"
+	path += "/" + escapeURLString(mkey)
+
+	err = delete(c, HTTPMethod, path)
+	return
+}
+
+// ReadSystemReplacemsgWebproxy API operation for FortiOS gets the Webproxy
+// with the specified index value.
+// Returns the requested Webproxy value when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the system.replacemsg - webproxy chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) ReadSystemReplacemsgWebproxy(mkey string) (mapTmp map[string]interface{}, err error) {
+	HTTPMethod := "GET"
+	path := "/api/v2/cmdb/system.replacemsg/webproxy"
+	path += "/" + escapeURLString(mkey)
+
+	mapTmp, err = read(c, HTTPMethod, path, false)
+	return
+}
+
