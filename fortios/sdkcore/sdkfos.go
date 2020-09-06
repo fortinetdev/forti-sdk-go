f1fadd2a8a8bdfd0310d705e17bb1fb12a116e2e
Frank Shen <fshen01@fortinet.com>
2020-09-07 01:18:37 +0800

    Support WirelessControllerBonjourProfile

--- fortios/sdkcore/sdkfos.go
+++ fortios/sdkcore/sdkfos.go
@@ -18016,3 +18016,57 @@ func (c *FortiSDKClient) ReadWirelessControllerSetting(mkey string) (mapTmp map[
 	return
 }
 
+// CreateWirelessControllerBonjourProfile API operation for FortiOS creates a new Bonjour Profile.
+// Returns the index value of the Bonjour Profile and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the wireless-controller - bonjour-profile chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) CreateWirelessControllerBonjourProfile(params *map[string]interface{}) (output map[string]interface{}, err error) {
+
+	HTTPMethod := "POST"
+	path := "/api/v2/cmdb/wireless-controller/bonjour-profile"
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// UpdateWirelessControllerBonjourProfile API operation for FortiOS updates the specified Bonjour Profile.
+// Returns the index value of the Bonjour Profile and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the wireless-controller - bonjour-profile chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) UpdateWirelessControllerBonjourProfile(params *map[string]interface{}, mkey string) (output map[string]interface{}, err error) {
+	HTTPMethod := "PUT"
+	path := "/api/v2/cmdb/wireless-controller/bonjour-profile"
+	path += "/" + escapeURLString(mkey)
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// DeleteWirelessControllerBonjourProfile API operation for FortiOS deletes the specified Bonjour Profile.
+// Returns error for service API and SDK errors.
+// See the wireless-controller - bonjour-profile chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) DeleteWirelessControllerBonjourProfile(mkey string) (err error) {
+	HTTPMethod := "DELETE"
+	path := "/api/v2/cmdb/wireless-controller/bonjour-profile"
+	path += "/" + escapeURLString(mkey)
+
+	err = delete(c, HTTPMethod, path)
+	return
+}
+
+// ReadWirelessControllerBonjourProfile API operation for FortiOS gets the Bonjour Profile
+// with the specified index value.
+// Returns the requested Bonjour Profile value when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the wireless-controller - bonjour-profile chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) ReadWirelessControllerBonjourProfile(mkey string) (mapTmp map[string]interface{}, err error) {
+	HTTPMethod := "GET"
+	path := "/api/v2/cmdb/wireless-controller/bonjour-profile"
+	path += "/" + escapeURLString(mkey)
+
+	mapTmp, err = read(c, HTTPMethod, path, false)
+	return
+}
+
