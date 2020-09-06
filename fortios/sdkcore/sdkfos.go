8d25135c3a6a2f7af9e495df154bb5d4af519eb8
Frank Shen <fshen01@fortinet.com>
2020-09-07 01:15:39 +0800

    Support SystemReplacemsgDeviceDetectionPortal

--- fortios/sdkcore/sdkfos.go
+++ fortios/sdkcore/sdkfos.go
@@ -16906,3 +16906,57 @@ func (c *FortiSDKClient) ReadSystemReplacemsgEc(mkey string) (mapTmp map[string]
 	return
 }
 
+// CreateSystemReplacemsgDeviceDetectionPortal API operation for FortiOS creates a new Device Detection Portal.
+// Returns the index value of the Device Detection Portal and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the system.replacemsg - device-detection-portal chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) CreateSystemReplacemsgDeviceDetectionPortal(params *map[string]interface{}) (output map[string]interface{}, err error) {
+
+	HTTPMethod := "POST"
+	path := "/api/v2/cmdb/system.replacemsg/device-detection-portal"
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// UpdateSystemReplacemsgDeviceDetectionPortal API operation for FortiOS updates the specified Device Detection Portal.
+// Returns the index value of the Device Detection Portal and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the system.replacemsg - device-detection-portal chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) UpdateSystemReplacemsgDeviceDetectionPortal(params *map[string]interface{}, mkey string) (output map[string]interface{}, err error) {
+	HTTPMethod := "PUT"
+	path := "/api/v2/cmdb/system.replacemsg/device-detection-portal"
+	path += "/" + escapeURLString(mkey)
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// DeleteSystemReplacemsgDeviceDetectionPortal API operation for FortiOS deletes the specified Device Detection Portal.
+// Returns error for service API and SDK errors.
+// See the system.replacemsg - device-detection-portal chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) DeleteSystemReplacemsgDeviceDetectionPortal(mkey string) (err error) {
+	HTTPMethod := "DELETE"
+	path := "/api/v2/cmdb/system.replacemsg/device-detection-portal"
+	path += "/" + escapeURLString(mkey)
+
+	err = delete(c, HTTPMethod, path)
+	return
+}
+
+// ReadSystemReplacemsgDeviceDetectionPortal API operation for FortiOS gets the Device Detection Portal
+// with the specified index value.
+// Returns the requested Device Detection Portal value when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the system.replacemsg - device-detection-portal chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) ReadSystemReplacemsgDeviceDetectionPortal(mkey string) (mapTmp map[string]interface{}, err error) {
+	HTTPMethod := "GET"
+	path := "/api/v2/cmdb/system.replacemsg/device-detection-portal"
+	path += "/" + escapeURLString(mkey)
+
+	mapTmp, err = read(c, HTTPMethod, path, false)
+	return
+}
+
