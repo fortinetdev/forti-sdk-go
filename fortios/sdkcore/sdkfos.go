9baf9a6bc28092a3aea8979980ffff5439aba129
Frank Shen <fshen01@fortinet.com>
2020-09-07 01:19:41 +0800

    Support WirelessControllerQosProfile

--- fortios/sdkcore/sdkfos.go
+++ fortios/sdkcore/sdkfos.go
@@ -18448,3 +18448,57 @@ func (c *FortiSDKClient) ReadWirelessControllerWtpGroup(mkey string) (mapTmp map
 	return
 }
 
+// CreateWirelessControllerQosProfile API operation for FortiOS creates a new Qos Profile.
+// Returns the index value of the Qos Profile and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the wireless-controller - qos-profile chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) CreateWirelessControllerQosProfile(params *map[string]interface{}) (output map[string]interface{}, err error) {
+
+	HTTPMethod := "POST"
+	path := "/api/v2/cmdb/wireless-controller/qos-profile"
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// UpdateWirelessControllerQosProfile API operation for FortiOS updates the specified Qos Profile.
+// Returns the index value of the Qos Profile and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the wireless-controller - qos-profile chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) UpdateWirelessControllerQosProfile(params *map[string]interface{}, mkey string) (output map[string]interface{}, err error) {
+	HTTPMethod := "PUT"
+	path := "/api/v2/cmdb/wireless-controller/qos-profile"
+	path += "/" + escapeURLString(mkey)
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// DeleteWirelessControllerQosProfile API operation for FortiOS deletes the specified Qos Profile.
+// Returns error for service API and SDK errors.
+// See the wireless-controller - qos-profile chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) DeleteWirelessControllerQosProfile(mkey string) (err error) {
+	HTTPMethod := "DELETE"
+	path := "/api/v2/cmdb/wireless-controller/qos-profile"
+	path += "/" + escapeURLString(mkey)
+
+	err = delete(c, HTTPMethod, path)
+	return
+}
+
+// ReadWirelessControllerQosProfile API operation for FortiOS gets the Qos Profile
+// with the specified index value.
+// Returns the requested Qos Profile value when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the wireless-controller - qos-profile chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) ReadWirelessControllerQosProfile(mkey string) (mapTmp map[string]interface{}, err error) {
+	HTTPMethod := "GET"
+	path := "/api/v2/cmdb/wireless-controller/qos-profile"
+	path += "/" + escapeURLString(mkey)
+
+	mapTmp, err = read(c, HTTPMethod, path, false)
+	return
+}
+
