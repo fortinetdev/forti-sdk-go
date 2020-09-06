50a28b2f40e11e8ba1580415ecfb11795b7d28b3
Frank Shen <fshen01@fortinet.com>
2020-09-07 01:14:43 +0800

    Support SystemReplacemsgFortiguardWf

--- fortios/sdkcore/sdkfos.go
+++ fortios/sdkcore/sdkfos.go
@@ -16528,3 +16528,57 @@ func (c *FortiSDKClient) ReadSystemReplacemsgNntp(mkey string) (mapTmp map[strin
 	return
 }
 
+// CreateSystemReplacemsgFortiguardWf API operation for FortiOS creates a new Fortiguard Wf.
+// Returns the index value of the Fortiguard Wf and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the system.replacemsg - fortiguard-wf chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) CreateSystemReplacemsgFortiguardWf(params *map[string]interface{}) (output map[string]interface{}, err error) {
+
+	HTTPMethod := "POST"
+	path := "/api/v2/cmdb/system.replacemsg/fortiguard-wf"
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// UpdateSystemReplacemsgFortiguardWf API operation for FortiOS updates the specified Fortiguard Wf.
+// Returns the index value of the Fortiguard Wf and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the system.replacemsg - fortiguard-wf chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) UpdateSystemReplacemsgFortiguardWf(params *map[string]interface{}, mkey string) (output map[string]interface{}, err error) {
+	HTTPMethod := "PUT"
+	path := "/api/v2/cmdb/system.replacemsg/fortiguard-wf"
+	path += "/" + escapeURLString(mkey)
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// DeleteSystemReplacemsgFortiguardWf API operation for FortiOS deletes the specified Fortiguard Wf.
+// Returns error for service API and SDK errors.
+// See the system.replacemsg - fortiguard-wf chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) DeleteSystemReplacemsgFortiguardWf(mkey string) (err error) {
+	HTTPMethod := "DELETE"
+	path := "/api/v2/cmdb/system.replacemsg/fortiguard-wf"
+	path += "/" + escapeURLString(mkey)
+
+	err = delete(c, HTTPMethod, path)
+	return
+}
+
+// ReadSystemReplacemsgFortiguardWf API operation for FortiOS gets the Fortiguard Wf
+// with the specified index value.
+// Returns the requested Fortiguard Wf value when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the system.replacemsg - fortiguard-wf chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) ReadSystemReplacemsgFortiguardWf(mkey string) (mapTmp map[string]interface{}, err error) {
+	HTTPMethod := "GET"
+	path := "/api/v2/cmdb/system.replacemsg/fortiguard-wf"
+	path += "/" + escapeURLString(mkey)
+
+	mapTmp, err = read(c, HTTPMethod, path, false)
+	return
+}
+
