967185ecbbcabeb6c9e6b59dff5d2df4df0b7926
Frank Shen <fshen01@fortinet.com>
2020-09-07 01:21:26 +0800

    Support SwitchControllerSystem

--- fortios/sdkcore/sdkfos.go
+++ fortios/sdkcore/sdkfos.go
@@ -19102,3 +19102,39 @@ func (c *FortiSDKClient) ReadSwitchControllerGlobal(mkey string) (mapTmp map[str
 }
 
 
+// UpdateSwitchControllerSystem API operation for FortiOS updates the specified System.
+// Returns the index value of the System and execution result when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the switch-controller - system chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) UpdateSwitchControllerSystem(params *map[string]interface{}, mkey string) (output map[string]interface{}, err error) {
+	HTTPMethod := "PUT"
+	path := "/api/v2/cmdb/switch-controller/system"
+	output = make(map[string]interface{})
+
+	err = createUpdate(c, HTTPMethod, path, params, output)
+	return
+}
+
+// DeleteSwitchControllerSystem API operation for FortiOS deletes the specified System.
+// Returns error for service API and SDK errors.
+// See the switch-controller - system chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) DeleteSwitchControllerSystem(mkey string) (err error) {
+
+	//No unset API for switch-controller - system
+	return
+}
+
+// ReadSwitchControllerSystem API operation for FortiOS gets the System
+// with the specified index value.
+// Returns the requested System value when the request executes successfully.
+// Returns error for service API and SDK errors.
+// See the switch-controller - system chapter in the FortiOS Handbook - CLI Reference.
+func (c *FortiSDKClient) ReadSwitchControllerSystem(mkey string) (mapTmp map[string]interface{}, err error) {
+	HTTPMethod := "GET"
+	path := "/api/v2/cmdb/switch-controller/system"
+
+	mapTmp, err = read(c, HTTPMethod, path, true)
+	return
+}
+
+
