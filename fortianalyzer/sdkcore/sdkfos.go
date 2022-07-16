ade4c648526847a4a76c6080aec0f66d48150a41
Xing <lix@fortinet.com>
2022-07-15 17:24:04 -0700

    Support FortiManager v7.0.4-v7.2.0; Fix slash convert issue;

--- fortianalyzer/sdkcore/sdkfos.go
+++ fortianalyzer/sdkcore/sdkfos.go
@@ -2,6 +2,7 @@
 // Author: Hongbin Lu (@fgtdev-hblu), Xing Li (@lix-fortinet)
 // Documentation:
 // Hongbin Lu (@fgtdev-hblu), Xing Li (@lix-fortinet),
+// Liang Liu (@MaxxLiu22), Yue Wang (@yuew-ftnt)
 
 // Description: Description: SDK for FortiAnalyzer Provider
 
